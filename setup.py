6b18b36411dec9b8f4f6f3aeb355b316da104f2e
Ed Burrows <burrowsej@gmail.com>
2023-02-16 15:04:59 +0000

    floor for dependencies and rerun freeze

--- setup.py
+++ setup.py
@@ -5,34 +5,34 @@ __description__ = "Environmental Land Management Geospatial Analysis"
 __version__ = "0.1"
 
 REQUIRED_PACKAGES = [
-    "pandas",
-    "seaborn",
-    "pydantic",
-    "pyarrow",
-    "rich",
-    "geopandas",
-    "pyspark",
-    "sedona",
-    "rioxarray",
-    "sentinelsat",
+    "pandas>=1.5.3",
+    "seaborn>=0.12.2",
+    "pydantic>=1.10.5",
+    "pyarrow>=10.0.1",
+    "rich>=12.5.1",
+    "geopandas>=0.12.2",
+    "pyspark>=3.3.2",
+    "sedona>=1.0.4",
+    "rioxarray>=0.13.3",
+    "sentinelsat>=1.1.1",
     "bs4",
 ]
 
 TEST_PACKAGES = [
-    "black",
-    "flake8",
-    "Flake8-pyproject",
-    "isort",
-    "pytest",
+    "black>=23.1.0",
+    "flake8>=6.0.0",
+    "Flake8-pyproject>=1.2.2",
+    "isort>=5.12.0",
+    "pytest>=7.2.1",
 ]
 
 DEV_PACKAGES = [
     *TEST_PACKAGES,
-    "pylint",
-    "ipython",
-    "ipykernel",
-    "ipywidgets",
-    "dbx",
+    "pylint>=2.16.2",
+    "ipython>=8.10.0",
+    "ipykernel>=6.21.2",
+    "ipywidgets>=8.0.4",
+    "dbx>=0.7.0",
 ]
 
 
