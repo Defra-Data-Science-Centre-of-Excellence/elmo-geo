2f4317470650179cb64097c98aa667341742e1b8
burrowsej <burrowsej@gmail.com>
2023-04-11 10:49:40 +0100

    Ewco (#7)
    * add new datasets and versions
    
    * finished ewco
    
    * add versions
    
    * fix flake8
    
    * .
    
    * fix isort
    
    * rm print
    
    * remove recent error datasets

--- elmo_geo/datasets.py
+++ elmo_geo/datasets.py
@@ -8,11 +8,30 @@ from typing import Any, Dict, List, Optional
 
 
 @dataclass
+class Version:
+    """A class for holding dataset information for intersecting with land parcels
+    Args:
+        name: The name of the version, usually date in the format `YYYY_MM_DD`
+        path_read: Path to the source data
+    """
+
+    name: str
+    path_read: str
+
+    def __post_init__(self):
+        if not self.name:
+            raise ValueError("`name` must be defined")
+        if not self.path_read:
+            raise ValueError("`path_read` must be defined")
+
+
+@dataclass
 class Dataset:
     """A class for holding dataset information for intersecting with land parcels
     Args:
         name: The name of the dataset (to be used in file naming)
-        path_read: Path to the source data
+        source: The source of the dataset, e.g. `rpa`
+        versions: Versions available - will be string formatted into path_read
         path_polygons: Path to save the intermediate cleaned data, defaults
             to `f"/mnt/lab/unrestricted/elmo/{self.name}/polygons.parquet"`
         path_output: Path to save the final cleaned data, defaults
@@ -26,7 +45,8 @@ class Dataset:
     """
 
     name: str
-    path_read: str
+    source: str
+    versions: List[Version]
     path_polygons: Optional[str] = None
     path_output: Optional[str] = None
     keep_cols: Optional[List[str]] = None
@@ -38,12 +58,17 @@ class Dataset:
         """Set the default values where undefined"""
         if not self.name:
             raise ValueError("`name` must be defined")
-        if not self.path_read:
-            raise ValueError("`path_read` must be defined")
+        if not self.source:
+            raise ValueError("`source` must be defined")
         if self.path_polygons is None:
-            self.path_polygons = f"/mnt/lab/unrestricted/elm/elmo/{self.name}/polygons.parquet"
+            self.path_polygons = (
+                f"/mnt/lab/unrestricted/elm/{self.source}/{self.name}"
+                + "/{version}/polygons.parquet"
+            )
         if self.path_output is None:
-            self.path_output = f"/mnt/lab/unrestricted/elm/elmo/{self.name}/output.parquet"
+            self.path_output = (
+                f"/mnt/lab/unrestricted/elm/{self.source}/{self.name}" + "/{version}/output.parquet"
+            )
         if self.keep_cols is None:
             self.keep_cols = []
         if self.rename_cols is None:
@@ -56,111 +81,263 @@ class Dataset:
 
 national_park = Dataset(
     name="national_park",
-    path_read="/dbfs/mnt/base/unrestricted/source_defra_data_services_platform/"
-    "dataset_national_parks/format_SHP_national_parks/LATEST_national_parks/"
-    "National_Parks_England.shp",
+    source="defra",
+    versions=[
+        Version(
+            name="2021_03_15",
+            path_read=(
+                "/dbfs/mnt/base/unrestricted/source_defra_data_services_platform"
+                "/dataset_national_parks/format_SHP_national_parks"
+                "/SNAPSHOT_2021_03_15_national_parks/National_Parks_England.shp"
+            ),
+        ),
+        Version(
+            name="2023_02_14",
+            path_read=(
+                "/dbfs/mnt/base/unrestricted/source_defra_data_services_platform"
+                "/dataset_national_parks/format_SHP_national_parks"
+                "/SNAPSHOT_2023_02_14_national_parks/National_Parks_England.shp"
+            ),
+        ),
+    ],
     keep_cols=["geometry", "name"],
     rename_cols={"name": "national_park_name"},
     output_coltypes={"national_park_name": "category"},
 )
 
+
 ramsar = Dataset(
     name="ramsar",
-    path_read="/dbfs/mnt/base/unrestricted/source_defra_data_services_platform/dataset_ramsar/"
-    "format_SHP_ramsar/LATEST_ramsar/Ramsar_England.shp",
+    source="defra",
+    versions=[
+        Version(
+            name="2021_03_16",
+            path_read=(
+                "/dbfs/mnt/base/unrestricted/source_defra_data_services_platform"
+                "/dataset_ramsar/format_SHP_ramsar/"
+                "SNAPSHOT_2021_03_16_ramsar/Ramsar_England.shp"
+            ),
+        ),
+        Version(
+            name="2023_02_14",
+            path_read=(
+                "/dbfs/mnt/base/unrestricted/source_defra_data_services_platform"
+                "/dataset_ramsar/format_SHP_ramsar/"
+                "SNAPSHOT_2023_02_14_ramsar/Ramsar_England.shp"
+            ),
+        ),
+    ],
     keep_cols=["geometry", "name"],
     rename_cols={"NAME": "ramsar_name"},
 )
 
-peatland = Dataset(
-    name="peatland",
-    path_read="/dbfs/mnt/migrated-landing/Natural England/Peaty_Soils_Location__England_.geojson",
-    keep_cols=["PCLASSDESC", "binary", "geometry"],
-    rename_cols={"binary": "geometry", "PCLASSDESC": "peat_desc"},
-    output_coltypes={"peat_desc": "category"},
+
+peaty_soils = Dataset(
+    name="peaty_soils",
+    source="defra",
+    versions=[
+        Version(
+            name="2021_03_24",
+            path_read=(
+                "/dbfs/mnt/base/unrestricted/source_defra_data_services_platform"
+                "/dataset_peaty_soils/format_GPKG_peaty_soils"
+                "/SNAPSHOT_2021_03_24_peaty_soils/refdata_owner.peaty_soils_location.gpkg"
+            ),
+        ),
+    ],
+    keep_cols=["pclassdesc", "geometry"],
+    output_coltypes={"pclassdesc": "category"},
 )
 
 national_character_areas = Dataset(
     name="national_character_areas",
-    path_read="/dbfs/mnt/base/unrestricted/source_defra_data_services_platform/"
-    "dataset_national_character_areas/format_SHP_national_character_areas/"
-    "LATEST_national_character_areas/National_Character_Areas___Natural_England.shp",
+    source="defra",
+    versions=[
+        Version(
+            name="2021_03_29",
+            path_read=(
+                "/dbfs/mnt/base/unrestricted/source_defra_data_services_platform"
+                "/dataset_national_character_areas/format_SHP_national_character_areas"
+                "/SNAPSHOT_2021_03_29_national_character_areas"
+                "/National_Character_Areas___Natural_England.shp"
+            ),
+        ),
+    ],
     keep_cols=["geometry", "NCA_Name"],
     rename_cols={"NCA_Name": "nca_name"},
 )
 
 sssi = Dataset(
     name="sssi",
-    path_read="/dbfs/mnt/base/unrestricted/source_defra_data_services_platform/"
-    "dataset_sites_of_special_scientific_interest/"
-    "format_SHP_sites_of_special_scientific_interest/LATEST_sites_of_special_scientific_interest/"
-    "Sites_of_Special_Scientific_Interest_England.shp",
+    source="defra",
+    versions=[
+        Version(
+            name="2021_03_17",
+            path_read=(
+                "/dbfs/mnt/base/unrestricted/source_defra_data_services_platform"
+                "/dataset_sites_of_special_scientific_interest"
+                "/format_SHP_sites_of_special_scientific_interest"
+                "/SNAPSHOT_2021_03_17_sites_of_special_scientific_interest"
+                "/Sites_of_Special_Scientific_Interest_England.shp"
+            ),
+        ),
+        Version(
+            name="2023_02_14",
+            path_read=(
+                "/dbfs/mnt/base/unrestricted/source_defra_data_services_platform"
+                "/dataset_sites_of_special_scientific_interest"
+                "/format_SHP_sites_of_special_scientific_interest"
+                "/SNAPSHOT_2023_02_14_sites_of_special_scientific_interest"
+                "/Sites_of_Special_Scientific_Interest_England.shp"
+            ),
+        ),
+    ],
     keep_cols=["geometry", "sssi_name", "status"],
     rename_cols={"status": "sssi_status"},
     output_coltypes={"sssi_status": "category"},
 )
 
+
 aonb = Dataset(
     name="aonb",
-    path_read="/dbfs/mnt/base/unrestricted/"
-    "source_defra_data_services_platform/"
-    "dataset_areas_of_outstanding_natural_beauty/"
-    "format_SHP_areas_of_outstanding_natural_beauty/"
-    "LATEST_areas_of_outstanding_natural_beauty/"
-    "Areas_of_Outstanding_Natural_Beauty_England.shp",
+    source="defra",
+    versions=[
+        Version(
+            name="2021_03_15",
+            path_read=(
+                "/dbfs/mnt/base/unrestricted/source_defra_data_services_platform"
+                "/dataset_areas_of_outstanding_natural_beauty"
+                "/format_SHP_areas_of_outstanding_natural_beauty"
+                "/SNAPSHOT_2021_03_15_areas_of_outstanding_natural_beauty"
+                "/Areas_of_Outstanding_Natural_Beauty_England.shp"
+            ),
+        ),
+        Version(
+            name="2023_02_14",
+            path_read=(
+                "/dbfs/mnt/base/unrestricted/source_defra_data_services_platform"
+                "/dataset_areas_of_outstanding_natural_beauty"
+                "/format_SHP_areas_of_outstanding_natural_beauty"
+                "/SNAPSHOT_2023_02_14_areas_of_outstanding_natural_beauty"
+                "/Areas_of_Outstanding_Natural_Beauty_England.shp"
+            ),
+        ),
+    ],
     keep_cols=["geometry", "name"],
     rename_cols={"name": "aonb_name"},
 )
 
 lfa = Dataset(
     name="lfa",
-    # path_read = "/dbfs/mnt/migrated-landing/General Access/lfa/refdata_owner.lfa.gpkg",
-    path_read="/dbfs/mnt/lab/unrestricted/edward.burrows@defra.gov.uk/refdata_owner_lfa.gpkg",
-    # path_read="/dbfs/mnt/lab/unrestricted/edward.burrows@defra.gov.uk/maglfa.shp"
+    source="rpa",
+    versions=[
+        Version(
+            name="2021_03_03",
+            path_read=(
+                "/dbfs/mnt/base/unrestricted/source_rpa_spatial_data_mart"
+                "/dataset_lfa_and_moorland_line/format_GPKG_lfa_and_moorland_line"
+                "/SNAPSHOT_2021_03_03_lfa_and_moorland_line/refdata_owner.lfa.gpkg"
+            ),
+        ),
+        Version(
+            name="2023_02_07",
+            path_read=(
+                "/dbfs/mnt/base/unrestricted/source_rpa_spatial_data_mart"
+                "/dataset_lfa_and_moorland_line/format_GPKG_lfa_and_moorland_line"
+                "/SNAPSHOT_2023_02_07_lfa_and_moorland_line/refdata_owner.lfa.zip"
+                "/refdata_owner.lfa/refdata_owner.lfa.gpkg"
+            ),
+        ),
+    ],
     keep_cols=["geometry", "name"],
     rename_cols={"name": "lfa_type"},
     output_coltypes={"lfa_type": "category"},
     read_kwargs={"engine": "pyogrio"},
 )
 
-moorland = Dataset(
-    name="moorland",
-    path_read="/dbfs/mnt/lab/unrestricted/edward.burrows@defra.gov.uk/magmoor.shp",
-    keep_cols=["geometry", "NAME"],
-    rename_cols={"NAME": "moorland"},
-    output_coltypes={"moorland": "category"},
-)
-
 region = Dataset(
     name="region",
-    path_read="/dbfs/mnt/migrated-landing/Office of National Statistics/"
-    "Regions__December_2021__EN_BFC.geojson",
+    source="ons",
+    versions=[
+        Version(
+            name="2021_12_01",
+            path_read=(
+                "/dbfs/mnt/migrated-landing/Office of National Statistics"
+                "/Regions__December_2021__EN_BFC.geojson"
+            ),
+        ),
+    ],
     keep_cols=["geometry", "RGN21NM"],
     rename_cols={"RGN21NM": "region"},
     output_coltypes={"region": "category"},
 )
 
 commons = Dataset(
-    name="commons",
-    path_read="/dbfs/mnt/migrated-landing/General Access/CRoWAct2000Section4ConclRegCommonLand/SHP/"
-    "Crow_Act_2000_Section_4_Conclusive_Registered_Common_Land_England.shp",
+    name="common_land",
+    source="rpa",
+    versions=[
+        Version(
+            name="2021_03_05",
+            path_read=(
+                "/dbfs/mnt/base/unrestricted/source_rpa_spatial_data_mart"
+                "/dataset_registered_common_land_bps_layer"
+                "/format_GPKG_registered_common_land_bps_layer"
+                "/SNAPSHOT_2021_03_05_registered_common_land_bps_layer/RCL.gpkg"
+            ),
+        ),
+        Version(
+            name="2023_02_07",
+            path_read=(
+                "/dbfs/mnt/base/unrestricted/source_rpa_spatial_data_mart"
+                "/dataset_registered_common_land_bps_layer"
+                "/format_GPKG_registered_common_land_bps_layer"
+                "/SNAPSHOT_2023_02_07_registered_common_land_bps_layer"
+                "/refdata_owner.rcl.zip/refdata_owner.rcl/refdata_owner.rcl.gpkg"
+            ),
+        ),
+    ],
     keep_cols=["geometry", "cl_number", "name"],
     rename_cols={"cl_number": "common_number", "name": "common_name"},
 )
 
 flood_risk_areas = Dataset(
     name="flood_risk_areas",
-    path_read="/dbfs/mnt/base/unrestricted/source_defra_data_services_platform/"
-    "dataset_flood_risk_areas/format_SHP_flood_risk_areas/LATEST_flood_risk_areas/"
-    "Flood_Risk_Areas.shp",
+    source="defra",
+    versions=[
+        Version(
+            name="2023_02_14",
+            path_read=(
+                "/dbfs/mnt/base/unrestricted/source_defra_data_services_platform"
+                "/dataset_flood_risk_areas/format_SHP_flood_risk_areas"
+                "/SNAPSHOT_2023_02_14_flood_risk_areas/Flood_Risk_Areas.shp"
+            ),
+        ),
+        Version(
+            name="2021_03_18",
+            path_read=(
+                "/dbfs/mnt/base/unrestricted/source_defra_data_services_platform"
+                "/dataset_flood_risk_areas/format_SHP_flood_risk_areas"
+                "/SNAPSHOT_2021_03_18_flood_risk_areas/Flood_Risk_Areas.shp"
+            ),
+        ),
+    ],
     keep_cols=["geometry", "flood_sour", "fra_name", "fra_id"],
     output_coltypes={"flood_sour": "category"},
 )
 
-ewco_red_squirrel = Dataset(
-    name="ewco_red_squirrel",
-    path_read="/dbfs/mnt/lab/unrestricted/elm/elmo/ewco_red_squirrel/"
-    "EWCO_Biodiversity_-_Priority_Species_-_Red_Squirrel_-_Woodland_Creation.shp",
+red_squirrel = Dataset(
+    name="red_squirrel",
+    source="ewco",
+    versions=[
+        Version(
+            name="2022_10_18",
+            path_read=(
+                "/dbfs/mnt/lab/unrestricted/elm_data/ewco/red_squirrel"
+                "/2022_10_18"
+                "/EWCO_Biodiversity___Priority_Species___Red_Squirrel___Woodland_Creation.shp"
+            ),
+        ),
+    ],
     keep_cols=["geometry", "status", "cswcm_pnts", "ewco_val", "sitename", "cat"],
     output_coltypes={
         "sitename": "category",
@@ -170,10 +347,18 @@ ewco_red_squirrel = Dataset(
     },
 )
 
-ewco_priority_habitat_network = Dataset(
-    name="ewco_priority_habitat_network",
-    path_read="/dbfs/mnt/lab/unrestricted/elm/elmo/ewco_priority_habitat_network/"
-    "EWCO_Biodiversity_Priority_Habitat_Network.shp",
+priority_habitat_network = Dataset(
+    name="priority_habitat_network",
+    source="ewco",
+    versions=[
+        Version(
+            name="2022_10_06",
+            path_read=(
+                "/dbfs/mnt/lab/unrestricted/elm_data/ewco/priority_habitat_network/"
+                "2022_10_06/EWCO_Biodiversity___Priority_Habitat_Network.shp"
+            ),
+        ),
+    ],
     keep_cols=["geometry", "csht_pnts", "cswcm_pnts", "ewco_val", "cat"],
     output_coltypes={
         "cat": "category",
@@ -182,10 +367,83 @@ ewco_priority_habitat_network = Dataset(
     },
 )
 
-ewco_close_to_settlements = Dataset(
-    name="ewco_close_to_settlements",
-    path_read="/dbfs/mnt/lab/unrestricted/elm/elmo/ewco_close_to_settlements/EWCO_-_NfC_Social.shp",
+nfc_social = Dataset(
+    name="nfc_social",
+    source="ewco",
+    versions=[
+        Version(
+            name="2022_03_14",
+            path_read=(
+                "/dbfs/mnt/lab/unrestricted/elm_data/ewco/nfc_social"
+                "/2022_03_14/EWCO___NfC_Social.shp"
+            ),
+        ),
+    ],
     keep_cols=["geometry", "status"],
+    output_coltypes={"status": "category"},
+)
+
+water_quality = Dataset(
+    name="water_quality",
+    source="ewco",
+    versions=[
+        Version(
+            name="2023_02_27",
+            path_read=(
+                "/dbfs/mnt/lab/unrestricted/elm_data/ewco/water_quality/"
+                "2023_02_27/EWCO__E2_80_93_Water_Quality.shp"
+            ),
+        ),
+    ],
+    keep_cols=["geometry", "cat"],
+    output_coltypes={"cat": "category"},
+)
+
+flood_risk_management = Dataset(
+    name="flood_risk_management",
+    source="ewco",
+    versions=[
+        Version(
+            name="2023_02_24",
+            path_read=(
+                "/dbfs/mnt/lab/unrestricted/elm_data/ewco/flood_risk_management/"
+                "2023_02_24/EWCO___Flood_Risk_Management.shp"
+            ),
+        ),
+    ],
+    keep_cols=["geometry", "LANDSCAPE"],
+    rename_cols={"LANDSCAPE": "cat"},
+    output_coltypes={"cat": "category"},
+)
+
+keeping_rivers_cool_riparian_buffers = Dataset(
+    name="keeping_rivers_cool_riparian_buffers",
+    source="ewco",
+    versions=[
+        Version(
+            name="2023_03_03",
+            path_read=(
+                "/dbfs/mnt/lab/unrestricted/elm_data/ewco/keeping_rivers_cool_riparian_buffers/"
+                "2023_03_03/EWCO___Keeping_Rivers_Cool_Riparian_Buffers.shp"
+            ),
+        ),
+    ],
+    keep_cols=["geometry", "OBJECTID"],
+)
+
+nfc_ammonia_emmissions = Dataset(
+    name="nfc_ammonia_emmissions",
+    source="ewco",
+    versions=[
+        Version(
+            name="2022_03_14",
+            path_read=(
+                "/dbfs/mnt/lab/unrestricted/elm_data/ewco/nfc_ammonia_emmissions/"
+                "2022_03_14/EWCO___NfC_Ammonia_Emissions_Capture_for_SSSI_Protection.shp"
+            ),
+        ),
+    ],
+    keep_cols=["geometry", "status", "pnts"],
     output_coltypes={"status": "category"},
 )
 
@@ -193,17 +451,20 @@ ewco_close_to_settlements = Dataset(
 datasets = [
     national_park,
     ramsar,
-    peatland,
+    peaty_soils,
     national_character_areas,
     sssi,
     aonb,
     lfa,
-    moorland,
     region,
     commons,
     flood_risk_areas,
-    ewco_red_squirrel,
-    ewco_priority_habitat_network,
-    ewco_close_to_settlements,
+    red_squirrel,
+    priority_habitat_network,
+    nfc_social,
+    water_quality,
+    flood_risk_management,
+    keeping_rivers_cool_riparian_buffers,
+    nfc_ammonia_emmissions,
 ]
 """A list of all defined datasets"""
--- notebooks/process_dataset.py
+++ notebooks/process_dataset.py
@@ -3,78 +3,95 @@
 # MAGIC # Processing vector data and calculating intersections with land parcels
 # MAGIC This notebook is used to clean up vector datasets, and to join them with the land parcels
 # MAGIC dataset to get the proportion of the land parcel intersecting with each feature
+
 # COMMAND ----------
+
 # MAGIC %load_ext autoreload
 # MAGIC %autoreload 2
+
 # COMMAND ----------
 
 import geopandas as gpd
-from pyspark.sql.functions import expr
+from pyspark.sql.functions import concat, expr
 from sedona.register import SedonaRegistrator
 
 from elmo_geo.datasets import datasets
 from elmo_geo.io import download_link
 from elmo_geo.joins import spatial_join
-from elmo_geo.preprocessing import preprocess_dataset
+from elmo_geo.log import LOG
+from elmo_geo.preprocessing import geometry_to_wkb, make_geometry_valid, transform_crs
 
 SedonaRegistrator.registerAll(spark)
 
 # COMMAND ----------
 
-name = "ewco_close_to_settlements"
-path_parcels = "/mnt/lab/unrestricted/elm/elmo/parcels/polygons.parquet"
+names = sorted([f"{d.source}/{d.name}/{v.name}" for d in datasets for v in d.versions])
+dbutils.widgets.dropdown("dataset", names[-1], names)
+_, name, version = dbutils.widgets.get("dataset").split("/")
+dataset = next(d for d in datasets if d.name == name)
+[print(k, v, sep=":\t") for k, v in dataset.__dict__.items()]
+path_parcels = "dbfs:/mnt/lab/unrestricted/elm_data/rpa/reference_parcels/2023_02_07.parquet"
 target_epsg = 27700
 n_partitions = 200
 simplify_tolerence: float = 0.5  # metres
 max_vertices: int = 256  # per polygon (row)
-
-dataset = next(d for d in datasets if d.name == name)
-print(dataset)
+path_read = next(v.path_read for v in dataset.versions if v.name == version)
 
 # COMMAND ----------
 
 # take a look at the raw data
-gpd.read_file(dataset.path_read, engine="pyogrio")
+gpd.read_file(path_read, engine="pyogrio", rows=8)
 
 # COMMAND ----------
 
 # process the dataset
-preprocess_dataset(
-    spark=spark,
-    path_read=dataset.path_read,
-    path_write=dataset.path_polygons,
-    keep_cols=dataset.keep_cols,
-    rename_cols=dataset.rename_cols,
-    target_epsg=target_epsg,
-    n_partitions=n_partitions,
-    geometry_col="geometry",
-    **dataset.read_kwargs,
+df = (
+    gpd.read_file(path_read, engine="pyogrio")
+    .explode(index_parts=False)
+    .pipe(transform_crs, target_epsg=27700)
+    .filter(dataset.keep_cols, axis="columns")
+    .rename(columns=dataset.rename_cols)
+    .pipe(make_geometry_valid)
+    .pipe(geometry_to_wkb)
 )
+
+LOG.info(f"Dataset has {df.size:,.0f} rows")
+LOG.info(f"Dataset has the following columns: {df.columns.tolist()}")
+(
+    spark.createDataFrame(df)
+    .repartition(n_partitions)
+    .write.format("parquet")
+    .save(dataset.path_polygons.format(version=version), mode="overwrite")
+)
+LOG.info(f"Saved preprocessed dataset to {dataset.path_polygons.format(version=version)}")
 
 # COMMAND ----------
 
 # take a look at the processed data
-df = spark.read.parquet(dataset.path_polygons)
+df = spark.read.parquet(dataset.path_polygons.format(version=version))
 df.display()
 
 # COMMAND ----------
 
-# further process the parcels dataset to ensure validity, simplify the vertices to a tolerence,
+# process the parcels dataset to ensure validity, simplify the vertices to a tolerence,
 # and subdivide large geometries
 df_parcels = (
     spark.read.parquet(path_parcels)
-    .withColumn("geometry", expr("ST_GeomFromWKB(hex(geometry))"))
+    .withColumn("id_parcel", concat("SHEET_ID", "PARCEL_ID"))
+    .withColumn("geometry", expr("ST_GeomFromWKB(wkb_geometry)"))
     .withColumn("geometry", expr("ST_MakeValid(geometry)"))
     .withColumn("geometry", expr(f"ST_SimplifyPreserveTopology(geometry, {simplify_tolerence})"))
     .withColumn("geometry", expr("ST_Force_2D(geometry)"))
     .withColumn("geometry", expr("ST_MakeValid(geometry)"))
-    .withColumnRenamed("id", "id_parcel")
+    .select("id_parcel", "geometry")
 )
+df_parcels.display()
+# COMMAND ----------
 
-# further process the feature dataset to ensure validity, simplify the vertices to a tolerence,
+# process the feature dataset to ensure validity, simplify the vertices to a tolerence,
 # and subdivide large geometries
 df_feature = (
-    spark.read.parquet(dataset.path_polygons)
+    spark.read.parquet(dataset.path_polygons.format(version=version))
     .withColumn("geometry", expr("ST_GeomFromWKB(hex(geometry))"))
     .withColumn("geometry", expr("ST_MakeValid(geometry)"))
     .withColumn("geometry", expr(f"ST_SimplifyPreserveTopology(geometry, {simplify_tolerence})"))
@@ -82,6 +99,9 @@ df_feature = (
     .withColumn("geometry", expr("ST_MakeValid(geometry)"))
     .withColumn("geometry", expr(f"ST_SubdivideExplode(geometry, {max_vertices})"))
 )
+df_feature.display()
+
+# COMMAND ----------
 
 # intersect the two datasets
 (
@@ -92,19 +112,19 @@ df_feature = (
         num_partitions=10000,
     )
     .write.format("parquet")
-    .save(dataset.path_output, mode="overwrite")
+    .save(dataset.path_output.format(version=version), mode="overwrite")
 )
 
 # COMMAND ----------
 
 # show results
-result = spark.read.parquet(dataset.path_output)
+result = spark.read.parquet(dataset.path_output.format(version=version))
 count = result.count()
-print(f"Rows: {count:,.0f}")
+LOG.info(f"Rows: {count:,.0f}")
 # check proportion is never > 1 - if it is might mean duplicate features int he dataset
 proportion_over_1 = (result.toPandas().proportion > 1.0).sum()
 if proportion_over_1:
-    print(
+    LOG.info(
         f"{proportion_over_1:,.0f} parcels have a feature "
         f"overlapping by a proportion > 1 ({proportion_over_1/count:%})"
     )
@@ -114,22 +134,29 @@ result.display()
 
 # download
 pandas_df = result.toPandas()
-path_feather = "/dbfs" + dataset.path_output.replace("output", dataset.name).replace(
-    ".parquet", ".feather"
-)
-path_csv = "/dbfs" + dataset.path_output.replace("output", dataset.name).replace(".parquet", ".csv")
+path_parquet = "/dbfs" + dataset.path_output.format(version=version).replace("output", dataset.name)
+path_feather = "/dbfs" + dataset.path_output.format(version=version).replace(
+    "output", dataset.name
+).replace(".parquet", ".feather")
+path_csv = "/dbfs" + dataset.path_output.format(version=version).replace(
+    "output", dataset.name
+).replace(".parquet", ".csv")
 
 # convert types
 for col, newtype in dataset.output_coltypes.items():
     pandas_df[col] = pandas_df[col].astype(newtype)
 
 # output
+pandas_df.to_parquet(path_parquet)
 pandas_df.to_feather(path_feather)
 pandas_df.to_csv(path_csv, index=False)
+displayHTML(download_link(spark, path_parquet))
 displayHTML(download_link(spark, path_feather))
 displayHTML(download_link(spark, path_csv))
 
 # COMMAND ----------
+
 # check for any issues causing prtoportion > 1
-pandas_df.sort_values("proportion", ascending=False).head(20)
+pandas_df.sort_values("proportion", ascending=False).head(8)
+
 # COMMAND ----------
