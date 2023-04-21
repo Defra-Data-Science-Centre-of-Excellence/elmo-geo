e98528826aecbff878be443f51d873be5dbfc3a8
Heather-Tymms <heather.tymms@defra.gov.uk>
2023-04-21 16:23:13 +0100

    changes from review

--- notebooks/process_tile_intersection.py
+++ notebooks/process_tile_intersection.py
@@ -1,74 +1,48 @@
 # Databricks notebook source
 # MAGIC %md
-# MAGIC # Processing vector data and calculating intersections with land parcels
-# MAGIC This notebook is used to clean up vector datasets, and to join them with the land parcels
-# MAGIC dataset to get the proportion of the land parcel intersecting with each feature
+# MAGIC # Intersecting tile data with land parcels
+# MAGIC This notebook is used to join sentinel tiles dataset with the land parcels
+# MAGIC dataset to get the proportion of the land parcel intersecting with each tile.
+# MAGIC It will then input the geometries back into the dataset so it can be ready for use
+# MAGICV in the calc_bare_soil_perc notebook.
 
 # COMMAND ----------
 
 # MAGIC %load_ext autoreload
 # MAGIC %autoreload 2
+# MAGIC %pip install  beautifulsoup4 lxml
 
 # COMMAND ----------
 
 import geopandas as gpd
-import numpy as np
 import pandas as pd
 import pyspark.sql.functions as F
 from pyspark.sql.functions import concat, expr
 from sedona.register import SedonaRegistrator
 
-from elmo_geo.datasets import datasets
-from elmo_geo.io import download_link
+from elmo_geo.datasets import tiles
 from elmo_geo.joins import spatial_join
 from elmo_geo.log import LOG
 from elmo_geo.preprocessing import geometry_to_wkb, make_geometry_valid, transform_crs
+from elmo_geo.sentinel import sentinel_tiles
 
 SedonaRegistrator.registerAll(spark)
 
 # COMMAND ----------
 
-names = sorted([f"{d.source}/{d.name}/{v.name}" for d in datasets for v in d.versions])
-dbutils.widgets.dropdown("dataset", names[-1], names)
-_, name, version = dbutils.widgets.get("dataset").split("/")
-dataset = next(d for d in datasets if d.name == name)
+names = sorted([f"{v.name}" for v in tiles.versions])
+dbutils.widgets.dropdown("version", names[-1], names)
+version = dbutils.widgets.get("dataset")
+dataset = tiles.copy()
 [print(k, v, sep=":\t") for k, v in dataset.__dict__.items()]
 path_parcels = "dbfs:/mnt/lab/unrestricted/elm_data/rpa/reference_parcels/2023_02_07.parquet"
+path_output = f"dbfs:/mnt/lab/unrestricted/elm/sentinel/tiles/{version}/parcels.parquet"
 target_epsg = 27700
 n_partitions = 200
 simplify_tolerence: float = 0.5  # metres
 max_vertices: int = 256  # per polygon (row)
-path_read = next(v.path_read for v in dataset.versions if v.name == version)
-required_tiles = [
-    "30UUA",
-    "30UUB",
-    "30UVA",
-    "30UVB",
-    "30UVC",
-    "30UVD",
-    "30UVE",
-    "30UVF",
-    "30UVG",
-    "30UWB",
-    "30UWC",
-    "30UWD",
-    "30UWE",
-    "30UWF",
-    "30UWG",
-    "30UXB",
-    "30UXC",
-    "30UXD",
-    "30UXE",
-    "30UXF",
-    "30UXG",
-    "30UYB",
-    "30UYC",
-    "30UYD",
-    "30UYE",
-    "31UCT",
-    "31UDT",
-    "31UDU",
-]
+path_read = next(v.path_read for v in tiles.versions if v.name == version)
+required_tiles = sentinel_tiles.copy()
 
 # COMMAND ----------
 
@@ -155,81 +129,46 @@ df_feature.display()
 
 # COMMAND ----------
 
-# show results
 result = spark.read.parquet(dataset.path_output.format(version=version))
+
+# COMMAND ----------
+
+result = result.filter(result.proportion > 0.99).select("tile", "id_parcel")
+df = (
+    result.join(df_parcels, ["id_parcel"], "left")
+    .select("id_parcel", "tile", "geometry")
+    .withColumn("geometry", expr("ST_AsBinary(geometry)"))
+)
+df.write.format("parquet").save(path_output, mode="overwrite")
+display(df)
+
+# COMMAND ----------
+
+# show results
 count = result.count()
 LOG.info(f"Rows: {count:,.0f}")
 # check proportion is never > 1 - if it is might mean duplicate features int he dataset
 proportion_over_1 = (result.toPandas().proportion > 1.0).sum()
 if proportion_over_1:
     LOG.info(
-        f"{proportion_over_1:,.0f} parcels have a feature "
-        f"overlapping by a proportion > 1 ({proportion_over_1/count:%})"
+        f"{proportion_over_1:,.0f} parcels overlap tiles "
+        f" by a proportion > 1 ({proportion_over_1/count:%})"
     )
 result.display()
 
 # COMMAND ----------
 
-# download
 pandas_df = result.toPandas()
-path_parquet = "/dbfs" + dataset.path_output.format(version=version).replace("output", dataset.name)
-path_feather = "/dbfs" + dataset.path_output.format(version=version).replace(
-    "output", dataset.name
-).replace(".parquet", ".feather")
-path_csv = "/dbfs" + dataset.path_output.format(version=version).replace(
-    "output", dataset.name
-).replace(".parquet", ".csv")
-
-# convert types
-for col, newtype in dataset.output_coltypes.items():
-    pandas_df[col] = pandas_df[col].astype(newtype)
-
-# output
-pandas_df.to_parquet(path_parquet)
-pandas_df.to_feather(path_feather)
-pandas_df.to_csv(path_csv, index=False)
-displayHTML(download_link(spark, path_parquet))
-displayHTML(download_link(spark, path_feather))
-displayHTML(download_link(spark, path_csv))
-
-# COMMAND ----------
-
 # check for any issues causing prtoportion > 1
-pandas_df.sort_values("proportion", ascending=False).head(8)
+pandas_df.sort_values("proportion", ascending=False).head(5)
 
 # COMMAND ----------
 
 # count number of proprtions less than 1
-conditions = [
-    (pandas_df["proportion"] == 0),
-    (pandas_df["proportion"] > 0) & (pandas_df["proportion"] <= 0.1),
-    (pandas_df["proportion"] > 0.1) & (pandas_df["proportion"] <= 0.2),
-    (pandas_df["proportion"] > 0.2) & (pandas_df["proportion"] <= 0.3),
-    (pandas_df["proportion"] > 0.3) & (pandas_df["proportion"] <= 0.4),
-    (pandas_df["proportion"] > 0.4) & (pandas_df["proportion"] <= 0.5),
-    (pandas_df["proportion"] > 0.5) & (pandas_df["proportion"] <= 0.6),
-    (pandas_df["proportion"] > 0.6) & (pandas_df["proportion"] <= 0.7),
-    (pandas_df["proportion"] > 0.7) & (pandas_df["proportion"] <= 0.8),
-    (pandas_df["proportion"] > 0.8) & (pandas_df["proportion"] <= 0.9),
-    (pandas_df["proportion"] > 0.9) & (pandas_df["proportion"] < 1),
-    (pandas_df["proportion"] == 1),
-]
-bin_values = [
-    "0",
-    "0-0.1",
-    "0.1-0.2",
-    "0.2-0.3",
-    "0.3-0.4",
-    "0.4-0.5",
-    "0.5-0.6",
-    "0.6-0.7",
-    "0.7-0.8",
-    "0.8-0.9",
-    "0.9-0.999",
-    "1",
-]
-
-pandas_df["proportion_groups"] = np.select(conditions, bin_values, default=0)
-pandas_df.groupby(by="proportion_groups").count()
+pandas_df["bins"] = pd.cut(
+    pandas_df["proportion"], [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+)
+proportion_df = pandas_df.groupby(by="bins").count()
+proportion_df
 
 # COMMAND ----------
