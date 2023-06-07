642bfbaa7b49dfe3f583f46b14db00844b14624a
Heather-Tymms <heather.tymms@defra.gov.uk>
2023-06-07 15:59:32 +0100

    adding extra step to choose tiles to run

--- notebooks/baresoil/02i_batch_process_ndvi.py
+++ notebooks/baresoil/02i_batch_process_ndvi.py
@@ -24,18 +24,28 @@ dbutils.widgets.multiselect("years", sentinel_years[-1], sentinel_years)
 
 # COMMAND ----------
 
-years: list = [str(n) for n in dbutils.widgets.get("years").split(",")]
+years: list = [str(n) for n in dbutils.widgets.get("years").split(",") if n != ""]
+if len(years) < 1:
+    raise ValueError("Please select at least one year to run")
+# adding just to retry for tiles that failed 3 times
+done_tiles = [
+    i.split("_")[1]
+    for i in os.listdir("/dbfs/mnt/lab/unrestricted/elm/elmo/baresoil/figures/")
+    for j in years
+    if i.startswith("ndvi_") and i.endswith(f"{j}.png")
+]
+tiles = [item for item in sentinel_tiles if item not in done_tiles][:6]
 notebook = "02_process_ndvi"
 
 LOG.info(
-    f"The tiles that are selected are: {sentinel_tiles}\n"
+    f"The tiles that are selected are: {tiles}\n"
     f"The years selected are: {years}\n"
     f"The notebook selected is: {notebook}"
 )
 
 # COMMAND ----------
 
-items = ({"year": y, "tile": t} for y, t in itertools.product(years, sentinel_tiles))
+items = ({"year": y, "tile": t} for y, t in itertools.product(years, tiles))
 
 n_cpu = os.cpu_count()
 LOG.info(f"Running on {n_cpu:,.0f} processes")
