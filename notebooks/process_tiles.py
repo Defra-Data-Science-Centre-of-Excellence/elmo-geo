9ce72ec2fa680c92570fc4140ff72144198a1ad7
Heather-Tymms <heather.tymms@defra.gov.uk>
2023-08-10 17:12:54 +0100

    isort amendments

--- notebooks/process_tiles.py
+++ notebooks/process_tiles.py
@@ -14,6 +14,7 @@
 
 import geopandas as gpd
 from shapely.ops import transform
+
 from elmo_geo.sentinel import sentinel_tiles
 
 path_source = "/dbfs/mnt/lab/unrestricted/elm_data/sentinel/sentinel_2_index_shapefile.shp"
