a3ba0b95e8d1b64866e3d097b95c4c3699474bd8
West <Andrew.West@defra.gov.uk>
2023-09-26 19:45:29 +0100

    check which `/dbfs:/`

---
+++ elmo_geo/utils/misc.py
@@ -0,0 +1,24 @@
+from elmo_geo.utils.log import LOG
+import subprocess
+
+
+def dbfs(f:str, spark_api:bool):
+  '''Confirm the filepath to spark_api or file_api
+  '''
+  if f.startswith('dbfs:/'):
+    if spark_api:
+      return f
+    else:
+      return f.replace('dbfs:/', '/dbfs/')
+  elif f.startswith('/dbfs/'):
+    if spark_api:
+      return f.replace('/dbfs/', 'dbfs:/')
+    else:
+      return f
+  else:
+    raise Exception(f'file must start with "dbfs:/" or "/dbfs/": {f}')
+
+
+def run(exc:str):
+  out = subprocess.run(exc, shell=True, check=True)
+  LOG.info(exc, out)
