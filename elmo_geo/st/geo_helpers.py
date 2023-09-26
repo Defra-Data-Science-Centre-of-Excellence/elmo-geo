86f75c9ffb7dca129a89061beafeba95b63db27b
West <Andrew.West@defra.gov.uk>
2023-09-26 20:26:02 +0100

    st_dump replacement

--- elmo_geo/st/geo_helpers.py
+++ elmo_geo/st/geo_helpers.py
@@ -24,3 +24,19 @@ def has_z(geoms: pd.Series) -> pd.Series:
 def is_valid(geoms: pd.Series) -> pd.Series:
     """Identify whether WKB geometries are valid"""
     return gpd.GeoSeries.from_wkb(geoms).is_valid
+
+
+
+# ST_Dump 
+@F.udf(T.ArrayType(T.BinaryType()))
+def st_dump_to_list(col):
+  gs = gpd.GeoSeries.from_wkb([col])
+  return gs.explode().wkb.tolist()
+
+def st_explode(sdf):
+  return (sdf
+    .withColumn('geometry', F.expr('ST_AsBinary(geometry)')
+    .withColumn('geometry', st_dump_to_list('geometry'))
+    .withColumn('geometry', F.explode('geometry'))
+    .withColumn('geometry', F.expr('ST_GeomFromWKB('geometry'))
+  )
