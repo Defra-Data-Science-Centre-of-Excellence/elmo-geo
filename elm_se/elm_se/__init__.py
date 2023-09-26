7e561f28281b6087f034272cb3e6efcf17287440
West <Andrew.West@defra.gov.uk>
2023-09-26 15:14:35 +0100

    all of elm_se to be organised and merged

---
+++ elm_se/elm_se/__init__.py
@@ -0,0 +1,10 @@
+'''ELM Spatial Eligibility'''
+
+# import subprocess
+
+# requirements = open('../requirements.txt').read().split()
+# subprocess.run(["pip", "install"] + requirements)
+
+
+from elm_se.log import LOG
+from elm_se.register import register
