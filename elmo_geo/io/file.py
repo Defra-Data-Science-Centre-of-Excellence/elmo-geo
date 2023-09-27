13cc84addb0faa5b35d8f2e95527256a40c567f0
West <Andrew.West@defra.gov.uk>
2023-09-27 14:09:53 +0100

    renamed and edited `to_gpq` functions

--- elmo_geo/io/file.py
+++ elmo_geo/io/file.py
@@ -1,38 +1,41 @@
-from elm_se.variables import CONDA_FOLDER
-from elm_se.types import SparkDataFrame
-from elm_se.utils import run
+from elmo_geo import LOG
+from elmo_geo.utils.types import SparkDataFrame
+from elmo_geo.utils.misc import run
+from elmo_geo.st.index import index_centroid, index_chipped
 
 
-def convert_file(f_in, f_out, layer):
-	run('/databricks/minconda/bin/ogr2ogr -progress -t_srs EPSG:27700 {f_out} {f_in} {layer}')
+
+def convert_file(f_in:str, f_out:str, layer:str):
+	out = run('/databricks/minconda/bin/ogr2ogr -progress -t_srs EPSG:27700 {f_out} {f_in} {layer}')
+	LOG.info(out)
 
 
-def to_gpq1(sdf:SparkDataFrame, sf_out:str):
-	# Index Partitioned
+def to_gpq_partitioned(sdf:SparkDataFrame, sf_out:str):
+	'''SparkDataFrame to GeoParquet, partitioned by BNG index
+	'''
 	(sdf
-		.transform(single_index, method='BNG', resolution=get_bng_resolution(sdf.count()))
+		.transform(index_centroid)
 		.write.format('geoparquet')
 		.save(sf_out, partitionBy='sindex')
 	)
 
-def to_gpq2(sdf, sf_out):
-	# Not Z-Ordered
+def to_gpq_sorted(sdf:SparkDataFrame, sf_out:str):
+	'''SparkDataFrame to GeoParquet, sorted by BNG index
+	'''
 	(sdf
-		.transform(single_index, method='BNG', resolution='1km')
-		.sort('bng')
+		.transform(index_centroid, resolution='1km')
+		.sort('sindex')
 		.write.format('geoparquet')
 		.save(sf_out)
 	)
 
-def to_gpq3(sdf, sf_out):
-	# Z-Ordered
+def to_gpq_zsorted(sdf:SparkDataFrame, sf_out:str):
+	'''SparkDataFrame to GeoParquet, sorted by geohash index
+	'''
 	(sdf
-		.transform(chipped_index, method='BNG', resolution='1km')
+		.transform(index, resolution='1km', index_join=chipped_index)
+		.withColumn('geohash', F.expr('ST_GeoHash(ST_FlipCoordinates(ST_Transform(geometry, "EPSG:27700", "EPSG:4326")))'))
 		.sort('geohash')
 		.write.format('geoparquet')
 		.save(sf_out)
 	)
-
-
-# Default to Variable BNG Index Partitioned
-to_gpq = to_gpq1
