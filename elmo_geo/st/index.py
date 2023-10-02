551fc9d76b819a0cdfcf6df58525724119c66844
aw-west-defra <andrew.west@defra.gov.uk>
2023-10-02 14:36:48 +0100

    split up `st`

---
+++ elmo_geo/st/index.py
@@ -0,0 +1,79 @@
+from pyspark.sql import functions as F
+
+from elmo_geo.st import join
+from elmo_geo.utils.dbr import spark
+from elmo_geo.utils.settings import BATCHSIZE
+from elmo_geo.utils.types import SparkDataFrame, Union
+
+
+def get_bng_resolution(n: int, /, target: int) -> str:
+    target = n // target
+    for resolution, cells in {
+        "100km": 91,
+        # '50km': 364,
+        # '20km': 2275,
+        "10km": 9100,
+        # '5km': 36400,
+        "1km": 910000,
+    }:
+        if target < cells:
+            break
+    return resolution
+
+
+def get_bng_grid(resolution: str) -> SparkDataFrame:
+    sf = "dbfs:/mnt/lab/unrestricted/elm/ods/os/bng_grid_{resolution}/2023.parquet"
+    return spark.read.parquet(sf)
+
+
+def get_grid(method: str, resolution: Union[str, int]) -> SparkDataFrame:
+    if method == "BNG":
+        return get_bng_grid(resolution=resolution)
+    elif method == "GeoHash":
+        raise NotImplementedError(method)
+    elif method == "H3":
+        raise NotImplementedError(method)
+    elif method == "S2":
+        raise NotImplementedError(method)
+    else:
+        methods = ["BNG", "GeoHash", "H3", "S2"]
+        raise TypeError(f"{method} not in {methods}")
+
+
+def multi_index(sdf: SparkDataFrame, grid: SparkDataFrame) -> SparkDataFrame:
+    return sdf.transform(join, grid, lsuffix="").drop("geometry_right")
+
+
+def centroid_index(sdf: SparkDataFrame, grid: SparkDataFrame) -> SparkDataFrame:
+    # there is the chance a centroid is on a grid line
+    return (
+        sdf.withColumnRenamed("geometry", "geometry_feature")
+        .withColumn("geometry", F.expr("ST_Centroid(geometry_feature)"))
+        .transform(join, grid, lsuffix="")
+        .drop("geometry", "geometry_right")
+        .withColumnRenamed("geometry_feature", "geometry")
+    )
+
+
+def chipped_index(sdf: SparkDataFrame, grid: SparkDataFrame) -> SparkDataFrame:
+    return (
+        sdf.transform(join, grid, lsuffix="")
+        .withColumn("geometry", F.expr("ST_Intersection(geometry, geometry_right)"))
+        .drop("geometry_right")
+    )
+
+
+def index(
+    sdf,
+    method: str = "BNG",
+    resolution: str = None,
+    index_join: callable = centroid_index,
+):
+    """Index a SparkDataFrame
+    this requires geometry_column == "geometry"
+    index_join: {centroid_index, chipped_index, multi_index}
+    """
+    if resolution is None:
+        resolution = get_bng_resolution(sdf.count(), target=BATCHSIZE)
+    grid = get_grid(method=method, resolution=resolution)
+    return index_join(sdf, grid=grid)
