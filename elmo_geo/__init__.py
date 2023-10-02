eb1911167974722221b7829b36dddd43992fd66e
obi.thompsonsargoni@defra.gov.uk <obi.thompsonsargoni@defra.gov.uk>
2023-10-02 16:54:44 +0100

    Adding the subprocesses import back in, but moving to the top of the file since the log import requires it.

--- elmo_geo/__init__.py
+++ elmo_geo/__init__.py
@@ -1,4 +1,23 @@
 """Functions for processing geospatial data using GeoVector cluster for ELMO"""
 
+import subprocess
+
+requires = [
+    # "numpy",
+    "rich",
+    # "pandas",
+    # "shapely",
+    # "geopandas",
+    # "rasterio",
+    # "xarray",
+    # "dbruntime",
+    # "folium",
+    # "matplotlib",
+    # "mapclassify",
+    # "seaborn",
+]
+
+subprocess.run(["pip", "install"] + requires)
+
 from elmo_geo.utils.log import LOG
 from elmo_geo.utils.register import register
