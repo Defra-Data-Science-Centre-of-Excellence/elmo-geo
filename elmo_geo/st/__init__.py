07215032fc0c521fa396e7cd354cd095d77bc010
aw-west-defra <andrew.west@defra.gov.uk>
2023-10-02 14:24:49 +0100

    added init and st_explode

---
+++ elmo_geo/st/__init__.py
@@ -0,0 +1,4 @@
+"""Vector (Spatial Types) functions for elmo_geo
+"""
+from .index import index
+from .join import sjoin
--- elmo_geo/st/udf.py
+++ elmo_geo/st/udf.py
@@ -1,10 +1,13 @@
 import geopandas as gpd
 import pandas as pd
-from pyspark.sql.functions import pandas_udf
+from pyspark.sql import functions as F
+from pyspark.sql import types as T
 from shapely.ops import transform
 
+from elmo_geo.utils.types import SparkDataFrame
 
-@pandas_udf("binary")
+
+@F.pandas_udf("binary")
 def remove_z(geoms: pd.Series) -> pd.Series:
     """Remove the z coordinate from WKB geometries"""
     return (
@@ -14,13 +17,29 @@ def remove_z(geoms: pd.Series) -> pd.Series:
     )
 
 
-@pandas_udf("boolean")
+@F.pandas_udf("boolean")
 def has_z(geoms: pd.Series) -> pd.Series:
     """Identify whether WKB geometries have a z coordinate"""
     return gpd.GeoSeries.from_wkb(geoms).has_z
 
 
-@pandas_udf("boolean")
+@F.pandas_udf("boolean")
 def is_valid(geoms: pd.Series) -> pd.Series:
     """Identify whether WKB geometries are valid"""
     return gpd.GeoSeries.from_wkb(geoms).is_valid
+
+
+# replace ST_Dump
+@F.udf(T.ArrayType(T.BinaryType()))
+def st_dump_to_list(col):
+    gs = gpd.GeoSeries.from_wkb([col])
+    return gs.explode().wkb.tolist()
+
+
+def st_explode(sdf: SparkDataFrame):
+    return (
+        sdf.withColumn("geometry", F.expr("ST_AsBinary(geometry)"))
+        .withColumn("geometry", st_dump_to_list("geometry"))
+        .withColumn("geometry", F.explode("geometry"))
+        .withColumn("geometry", F.expr("ST_GeomFromWKB(geometry)"))
+    )
