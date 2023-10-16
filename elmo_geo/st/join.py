0f3fa3546260d469170a4c29a8e09d6abf5c024c
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2023-10-16 12:16:39 +0100

    aw-desires' st module (#28)
    * all of elm_se to be organised and merged
    
    * moved most, deleted some notebooks
    
    * Renaming notebooks.
    
    * renamed elm_se.io to io2 for merging
    
    * moved modules
    
    * added __init__s
    
    * added import for dbr
    
    * refactored register
    
    * added some settings for our datasets
    
    * import LOG and register
    
    * replace imports
    
    * missed a couple imports
    
    * moved process_datasets to ingestion
    
    * edit imports in notebooks
    
    mine are to be archived and probably deleted.
    
    * Corrections and reorganisation of tree detection validation notebook
    
    * Corrections to imports
    
    Corrections required following changing imports in notebooks to points at new elmo_geo structure
    
    * Additional corrections to imports
    
    * Additional import correction
    
    * Setting logging level for py4j logger to stop generic 'Received command c on object id p0' log messages
    
    * Deleting py4j.java_gateway logging setting as this is superseded by the py4j setting
    
    * make fmt
    
    * make fmt but just .
    
    * flake8'd modules
    
    * extra make fmt was needed
    
    * `r` > `rs`
    
    * fixed flake8 checks for notebooks
    
    * removed elm_se folder
    
    * fmt and ignoring notebooks
    
    * added libs
    
    * misc utils; dbfs checker, check gtypes
    
    * multiselect widget
    
    * move geo_helpers to udf
    
    * st_explode
    
    * split up `st`
    
    * deprecated
    
    * depreciated and `make`
    
    * avoid circular imports
    
    * avoid circular sjoin definition
    
    * res.items()
    
    * remove chanegs to `Relict Hedges - Plot.py`
    
    ---------
    
    Co-authored-by: obi.thompsonsargoni@defra.gov.uk <obi.thompsonsargoni@defra.gov.uk>

---
+++ elmo_geo/st/join.py
@@ -0,0 +1,135 @@
+from pyspark.sql import functions as F
+from sedona.core.spatialOperator import JoinQuery
+from sedona.utils.adapter import Adapter
+
+from elmo_geo.io import load_missing
+
+# from elmo_geo import LOG
+from elmo_geo.utils.dbr import spark
+from elmo_geo.utils.types import SparkDataFrame
+
+
+def sjoin_rdd(
+    sdf_left: SparkDataFrame,
+    sdf_right: SparkDataFrame,
+    partitioning: str = "KDBTREE",
+    num_partitions: int = 200,
+    partition_right: bool = False,
+    index_type: str = "RTREE",
+    use_index: bool = True,
+    consider_boundary_itersection: bool = True,
+) -> SparkDataFrame:
+    """Spatial Join using RDD"""
+    # RDD
+    rdd_left = Adapter.toSpatialRdd(sdf_left, "geometry")
+    rdd_right = Adapter.toSpatialRdd(sdf_right, "geometry")
+    rdd_left.analyze()
+    rdd_right.analyze()
+    # Build Tree
+    if partition_right:
+        rdd_right.spatialPartitioning(partitioning=partitioning, num_partitions=num_partitions)
+        rdd_left.spatialPartitioning(rdd_right.getPartitioner())
+    else:
+        rdd_left.spatialPartitioning(partitioning=partitioning, num_partitions=num_partitions)
+        rdd_right.spatialPartitioning(rdd_left.getPartitioner())
+    if use_index:
+        rdd_left.buildIndex(indexType=index_type, buildIndexOnSpatialPartitionedRDD=True)
+    # Join
+    rdd = JoinQuery.SpatialJoinQueryFlat(
+        rdd_right,
+        rdd_left,
+        useIndex=use_index,
+        considerBoundaryIntersection=consider_boundary_itersection,
+    )
+    return Adapter.toDf(spatialPairRDD=rdd, sparkSession=spark)
+
+
+def sjoin_sql(
+    sdf_left: SparkDataFrame,
+    sdf_right: SparkDataFrame,
+    distance: float = 0,
+) -> SparkDataFrame:
+    """Spatial Join, only returning keys
+    Only suitable for minimal SparkDataFrames
+    left.select('id_left', 'geometry')
+    right.select('id_right', 'geometry')
+    """
+    # Distance Join
+    if 0 < distance:
+        sdf_left.withColumn("geometry", F.expr("ST_MakeValid(ST_Buffer(geometry, {distance}))"))
+    # Add to SQL
+    sdf_left.createOrReplaceTempView("left")
+    sdf_right.createOrReplaceTempView("right")
+    # Join
+    sdf = spark.sql(
+        """
+        SELECT id_left, id_right
+        FROM left, right
+        WHERE ST_Intersects(left.geometry, right.geometry)
+    """
+    )
+    # Remove from SQL
+    spark.sql("DROP TABLE left")
+    spark.sql("DROP TABLE right")
+    return sdf
+
+
+def sjoin(
+    sdf_left: SparkDataFrame,
+    sdf_right: SparkDataFrame,
+    how: str = "inner",
+    lsuffix: str = "_left",
+    rsuffix: str = "_right",
+    sjoin_fn: callable = sjoin_sql,
+    sjoin_kwargs: dict = {},
+) -> SparkDataFrame:
+    """Spatial Join with how
+    how: {inner, full, left, right}
+    sjoin: {elmo_geo.st.join.sjoin_sql, elmo_geo.st.join.sjoin_rdd}
+    """
+    # ID
+    sdf_left = sdf_left.withColumn("id_left", F.monotonically_increasing_id())
+    sdf_right = sdf_right.withColumn("id_right", F.monotonically_increasing_id())
+    # How
+    how_outer = ["outer", "outer_left", "outer_right", "anti", "anti_left", "anti_right"]
+    if how in how_outer:
+        raise NotImplementedError(how_outer)
+    how_left = "full" if how in ["left", "full"] else "inner"
+    how_right = "full" if how in ["right", "full"] else "inner"
+    # Spatial Join
+    sdf = sjoin_fn(
+        sdf_left.select("id_left", "geometry"),
+        sdf_right.select("id_right", "geometry"),
+        **sjoin_kwargs,
+    )
+    # Rename
+    for col in sdf_left.columns:
+        if col in sdf_right.columns:
+            sdf_left = sdf_left.withColumnRenamed(col, col + lsuffix)
+            sdf_right = sdf_right.withColumnRenamed(col, col + rsuffix)
+    geometry_left, geometry_right = f"left.geometry{lsuffix}", f"right.geometry{rsuffix}"
+    # Regular Join
+    return (
+        sdf.join(sdf_left.drop("geometry"), how=how_left, on="id_left")
+        .join(sdf_right.drop("geometry"), how=how_right, on="id_right")
+        .withColumn(geometry_left, load_missing(geometry_left))
+        .withColumn(geometry_right, load_missing(geometry_right))
+        .drop("id_left", "id_right")
+    )
+
+
+def overlap(
+    sdf_left: SparkDataFrame,
+    sdf_right: SparkDataFrame,
+    lsuffix: str = "_left",
+    rsuffix: str = "_right",
+    **kwargs,
+):
+    geometry_left, geometry_right = "geometry" + lsuffix, "geometry" + rsuffix
+    return (
+        sjoin(sdf_left, sdf_right, lsuffix=lsuffix, rsuffix=lsuffix, **kwargs)
+        .withColumn("geometry", F.expr(f"ST_Intersection({geometry_left}, {geometry_right})"))
+        # TODO: groupby
+        .withColumn("proportion", F.expr(f"ST_Area(geometry) / ST_Area({geometry_left})"))
+        .drop(geometry_left, geometry_right)
+    )
