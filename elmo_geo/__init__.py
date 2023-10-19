e35e5cf30f801c22694b895bc559d0c4ad7ad8d8
aw-west-defra <aw-west-defra>
2023-10-19 10:21:25 +0000

    force loading all submodules, which will check they work

--- elmo_geo/__init__.py
+++ elmo_geo/__init__.py
@@ -1,4 +1,6 @@
 """Functions for processing geospatial data using GeoVector cluster for ELMO"""
-
 from elmo_geo.utils.log import LOG
 from elmo_geo.utils.register import register
+from elmo_geo.utils.register import register_submodules as _register_submodules
+
+_register_submodules(__path__)
--- elmo_geo/utils/register.py
+++ elmo_geo/utils/register.py
@@ -1,19 +1,27 @@
-import sedona
+import pkgutil
 
 from elmo_geo import LOG
 from elmo_geo.utils.dbr import spark
 from elmo_geo.utils.types import SparkSession
 
-if sedona.version < "1.4.1":
-    from sedona.register import SedonaRegistrator
-
-    register_sedona = SedonaRegistrator.registerAll
-else:
+try:
     from sedona.spark import SedonaContext
 
     register_sedona = SedonaContext.create
+except Exception:
+    from sedona.register import SedonaRegistrator
+
+    register_sedona = SedonaRegistrator.registerAll
 
 
 def register(spark: SparkSession = spark):
     register_sedona(spark)
-    LOG.info(f"Registered: Sedona {sedona.version}")
+    LOG.info("Registered: Sedona")
+
+
+def register_submodules(path):
+    """Import all modules/submodule <file>.py to __init__.py
+    https://stackoverflow.com/a/3365846/10450752
+    """
+    for loader, module_name, _ in pkgutil.walk_packages(path):
+        globals()[module_name] = loader.find_module(module_name).load_module(module_name)
