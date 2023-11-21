ccc69db3d138a132988b93b7807013a5b71704f8
aw-west-defra <andrew.west@defra.gov.uk>
2023-11-21 17:24:10 +0000

    reduced inits

--- elmo_geo/__init__.py
+++ elmo_geo/__init__.py
@@ -1,5 +1,3 @@
 """Functions for processing geospatial data using GeoVector cluster for ELMO"""
 from elmo_geo.utils.log import LOG
-from elmo_geo.utils.register import register, register_submodules as _register_submodules
-
-_register_submodules(__path__)
+from elmo_geo.utils.register import register
--- elmo_geo/plot/__init__.py
+++
@@ -1,3 +0,0 @@
-import contextily as ctx
-
-osm_provider = ctx.providers.OrdnanceSurvey.Road(key="WxgUdETn6cy58WZkfwZ7wdMVLlt5eDsX")
