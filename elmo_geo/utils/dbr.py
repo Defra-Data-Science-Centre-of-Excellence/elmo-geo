1eb380cbe1e57f13af8c6593cb51763c57d55f8a
aw-west-defra <andrew.west@defra.gov.uk>
2024-02-19 18:56:11 +0000

    some issues

--- elmo_geo/utils/dbr.py
+++ elmo_geo/utils/dbr.py
@@ -1,8 +1,12 @@
 try:
-    from databricks.sdk.runtime import dbutils, spark
-except Exception:
-    from pyspark.dbutils import DBUtils
-    from pyspark.sql import SparkSession
+    from databricks.sdk.runtime import dbutils, display, displayHTML, spark  # noqa:F401
+except ImportError or ModuleNotFoundError:
+    try:
+        from pyspark.dbutils import DBUtils
+        from pyspark.sql import SparkSession
 
-    spark = SparkSession.getActiveSession()
-    dbutils = DBUtils(spark)
+        spark = SparkSession.builder.getOrCreate()
+        dbutils = DBUtils(spark)
+    except ImportError or ModuleNotFoundError:
+        spark, dbutils = None, None
+        ImportWarning("spark and dbutils are unavailable.")
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,15 +1,17 @@
+import contextily as ctx
+import matplotlib.pyplot as plt
 import pytest
-from elmo_geo import utils
 
+from elmo_geo.utils.ssl import no_ssl_verification
+
+
+@pytest.mark.without_cluster()
 def test_no_ssl_verification_ctx_basemap():
-    import contextily as ctx
-    import matplotlib.pyplot as plt
-
     try:
-        fig, ax = plt.subplots(figsize=figsize)
-        lims=(446450, 349550, 446650, 349850)
+        _, ax = plt.subplots()
+        lims = (446450, 349550, 446650, 349850)
         ax.set(xlim=[lims[0], lims[2]], ylim=[lims[1], lims[3]])
-        with utils.ssl.no_ssl_verification:
+        with no_ssl_verification:
             ctx.add_basemap(
                 ax=ax,
                 source=ctx.providers.Thunderforest.Landscape(
