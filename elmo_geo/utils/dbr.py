f7c62a555d1a8efc1e66ae8fbf83259ae07411cd
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2024-02-21 14:16:08 +0000

    semi-functioning dbutils

--- elmo_geo/utils/dbr.py
+++ elmo_geo/utils/dbr.py
@@ -1,6 +1,22 @@
 try:
     from databricks.sdk.runtime import dbutils, spark  # noqa:F401
 except Exception:
-    # TODO: make noop DBR
-    spark, dbutils = None, None
-    ImportWarning("dbr unavailable")
+    ImportWarning("noop dbr")
+    from databricks.sdk.core import Config
+    from databricks.sdk.credentials_provider import credentials_provider
+    from databricks.sdk.dbutils import RemoteDbUtils
+    from pyspark.sql import SparkSession
+
+    spark = SparkSession.builder.getOrCreate()
+
+    @credentials_provider("noop", [])
+    def noop_credentials(_: any):
+        return lambda: {}
+
+    dbutils = RemoteDbUtils(
+        Config(
+            host="http://localhost",
+            cluster_id="x",
+            credentials_provider=noop_credentials,
+        )
+    )
