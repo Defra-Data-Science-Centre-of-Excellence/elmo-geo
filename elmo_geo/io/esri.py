fce760ca117d0ecc6f62dd92c33163652b8551d8
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2024-02-21 12:35:01 +0000

    aw - 01 fix io (#51)
    * make clean python compiling
    
    * databrick-sdk
    
    * import more db
    
    * better SDF geometry tests
    
    * more settings for importing in elmo_geo
    
    * provider for os basemap
    
    should .env secrets importing be move to utils?
    
    * Ops, revert vscode settings
    
    * delete old files io2 to be split apart
    
    * create a link to download from dbfs
    
    * shorter conversion methods
    
    * ingest instead of download
    
    * convert a vector dataset to geoparquet
    
    * logging on saving a dataset
    
    * osm pbf alternative to overpass query
    
    * dash data ingestion
    
    * most useful io tools
    
    * make fmt
    
    * make fmt (#53)
    
    * obi's comment
    
    * idk
    
    * obi's comments
    
    * remove dataset/catalogue reference
    
    * Tests for to_sdf and function corrections
    
    * Formatting changes
    
    ---------
    
    Co-authored-by: obi.thompsonsargoni@defra.gov.uk <obi.thompsonsargoni@defra.gov.uk>

--- elmo_geo/io/esri.py
+++ elmo_geo/io/esri.py
@@ -1,10 +1,70 @@
-import subprocess
+"""REST API servers designed by ESRI
 
-from elmo_geo import LOG
+Used to ingest data from sources like Natural England and Office of National Statistics.
+"""
+
+from datetime import date
+
+import esridump  # noqa:F401
+import requests
+
+# from elmo_geo.io.datasets import append_to_catalogue
+from elmo_geo.io.file import convert_file
+from elmo_geo.utils.misc import sh_run
+from elmo_geo.utils.settings import FOLDER_STG
 
 
-def download_esri(url: str, path: str):
-    exc = f"esri2geojson {url} {path}"
-    out = subprocess.run(exc, shell=True, check=True)
-    # Add to datasets
-    LOG.info(out)
+def get_esri_name(url):
+    if "JJzESW51TqeY9uat" in url:
+        source = "ne"
+    elif "ESMARspQHYMw9BZ9" in url:
+        source = "ons"
+    dataset = url.split("/services/")[1].split("/FeatureServer")[0]
+    isodate = (
+        date.fromtimestamp(
+            esridump.EsriDumper(url).get_metadata()["editingInfo"]["dataLastEditDate"] / 1000
+        )
+        .isoformat()
+        .replace("-", "_")
+    )
+    return f"{source}-{dataset}-{isodate}"
+
+
+def ingest_esri(url, name=None):
+    """Download and Convert data stored on an ESRI server"""
+    if name is None:
+        name = get_esri_name(url)
+    f_in, f_out = FOLDER_STG + f"/{name}.geojson", FOLDER_STG + f"/{name}.parquet"
+    sh_run(f"esri2geojson '{url}' '{f_in}'")
+    convert_file(f_in, f_out)
+    # append_to_catalogue(
+    #     {
+    #         name: {
+    #             "url": url,
+    #             "filepath_tmp": f_in,
+    #             "filepath": f_out,
+    #             "function": "ingest_esri",
+    #         }
+    #     }
+    # )
+
+
+def esri_datasets(url):
+    """get a dict containing all avaiable datasets"""
+    r = requests.get(url + "?pjson").json()
+    return dict(
+        sorted(
+            {
+                d["name"]: {
+                    "url": d["url"],
+                    "function": "get_esri",
+                }
+                for d in r["services"]
+            }.items()
+        )
+    )
+
+
+def search_datasets(pattern, datasets):
+    """Filter dictionary for keys that contain the pattern"""
+    return {k: v for k, v in datasets.items() if pattern in k.lower()}
--- elmo_geo/io/osm.py
+++ elmo_geo/io/osm.py
@@ -1,33 +1,55 @@
-import osmnx
+"""OpenStreetMap
+Download and Ingest OSM data, there are 2 options;
+1. Download a single large file: planet.osm(.pbf) or a <region>.osm(.pbf) (pbf is the compression method)
+  - [planet.osm](https://planet.openstreetmap.org/)
+  - regions from [Geofabrik.de](https://download.geofabrik.de/)
+  - [Britain and Ireland](https://download.geofabrik.de/europe/britain-and-ireland-latest.osm.pbf)
+2. Download specific tags and bounds using Overpass API
+  - Try: [tag finder](https://tagfinder.osm.ch/)
+"""  # noqa:E501
 
-from elmo_geo.io.geometry import load_geometry
-from elmo_geo.utils.dbr import spark
-from elmo_geo.utils.misc import dbfs
-from elmo_geo.utils.settings import BATCHSIZE
+import osmnx  # noqa:F401
+
+# from elmo_geo.io.datasets import append_to_catalogue
+from elmo_geo.io.file import convert_file
+from elmo_geo.utils.misc import sh_run
+from elmo_geo.utils.settings import FOLDER_STG
 
 
-def download_osm(place, tags):
-    raise NotImplementedError("download_osm")
+def ingest_osm_pbf(url, name):
+    f_in, f_out = f"{FOLDER_STG}/{name}.osm.pbf", f"{FOLDER_STG}/{name}.parquet"
+    sh_run(f"wget {url} -O {f_in}")
+    convert_file(f_in, f_out)
+    # append_to_catalogue(
+    #     {
+    #         name: {
+    #             "url": url,
+    #             "filepath_tmp": f_in,
+    #             "filepath": f_out,
+    #             "function": "ingest_osm_pbf",
+    #         }
+    #     }
+    # )
 
 
-def read_osm(place, tags):
-    """Slowly read OSM data, then convert it to spark"""
+def ingest_osm_overpass(place, tags, name):
+    """Download OSM data using Overpass API, place and tags
+    Try: [tag finder](https://tagfinder.osm.ch/)
+    Example
+    ```py
+    today = datetime.now().strfmt('%Y_%m_%d')
+    ingest_osm_overpass(
+        place = 'England',
+        tags = {'wall': 'dry_stone'},
+        name = 'osm-dry_stone_wall-{today}',
+    )
+    ```
+    """
     osmnx.settings.cache_folder = "/databricks/driver/"
     osmnx.settings.timeout = 600
-
-    sdf = (
+    (
         osmnx.geometries_from_place(place, tags)
         .reset_index()[["osmid", *tags.keys(), "geometry"]]
-        .to_wkb()
-        .pipe(spark.createDataFrame)
+        .to_crs(epsg=27700)
+        .to_parquet(f"{FOLDER_STG}/{name}.parquet")
     )
-
-    return sdf.repartition(sdf.count() // BATCHSIZE + 1).withColumn(
-        "geometry", load_geometry("geometry", from_crs="EPSG:4326")
-    )
-
-
-def ingest_osm(f, place, tags):
-    sdf = read_osm(place, tags)
-    sdf.write.parquet(dbfs(f, True))
-    return sdf
