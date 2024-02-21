93f4aa16484553ea3a144867c525a351d17d20a0
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2024-02-21 13:45:16 +0000

    only import with cluster

--- tests/test_io.py
+++ tests/test_io.py
@@ -1,11 +1,11 @@
 import geopandas as gpd
 from shapely.geometry import Point
 
-from elmo_geo.io.convert import to_sdf
-from elmo_geo.utils.register import register
-
 
 def test_to_sf_geoseries():
+    from elmo_geo.io.convert import to_sdf
+    from elmo_geo.utils.register import register
+
     register()
     to_sdf(
         gpd.GeoSeries([Point(0, 0)]),
@@ -15,6 +15,9 @@ def test_to_sf_geoseries():
 
 
 def test_to_sf_basegeometry():
+    from elmo_geo.io.convert import to_sdf
+    from elmo_geo.utils.register import register
+
     register()
     to_sdf(
         Point(0, 0),
