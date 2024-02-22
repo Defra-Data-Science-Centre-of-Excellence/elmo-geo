1927ebebeeba6142266ddfcfe31f554b0ece7457
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2024-02-22 15:36:32 +0000

    tidied test

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,14 +1,11 @@
 import importlib
 from glob import glob
 
-# import pytest  # noqa:F401
-
 
 def test_dbr():
     from elmo_geo.utils.dbr import dbutils, spark  # noqa:F401
 
 
-# @pytest.mark.dbr
 def test_imports():
     files = glob("elmo_geo/**/*.py", recursive=True)
     for file in files:
