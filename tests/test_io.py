8525b038a23a8220dbb5269c25080b3936ff6c5b
obi.thompsonsargoni@defra.gov.uk <obi.thompsonsargoni@defra.gov.uk>
2024-03-04 18:22:37 +0000

    New test that fails. Checks for expected geometry column in output.

--- tests/test_io.py
+++ tests/test_io.py
@@ -15,6 +15,24 @@ def test_to_sf_geoseries():
         crs=27700,
     )
 
+@pytest.mark.dbr
+def test_to_gdf_sdf():
+    from elmo_geo.io.convert import to_gdf, to_sdf
+    from elmo_geo.utils.register import register
+
+    register()
+
+    gdf_in = gpd.GeoDataFrame({"geometry_test":[Point(0,0).wkb], "id":[0]})
+    sdf = to_sdf(gdf_in, column='geometry_test')
+
+    gdf_out = to_gdf(
+        gdf_in,
+        column="geometry_test",
+        crs=27700,
+    )
+
+    assert "geometry" in gdf_out.columns
+
 
 @pytest.mark.dbr
 def test_to_sf_basegeometry():
