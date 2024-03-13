64ab5d52d659455894f3d20bcfa78b4ec3ab967f
burrowsej <burrowsej>
2024-03-13 15:20:47 +0000

    fmt

--- elmo_geo/datasets/datasets.py
+++ elmo_geo/datasets/datasets.py
@@ -74,16 +74,14 @@ class Dataset:
         if self.read_kwargs is None:
             self.read_kwargs = {}
 
+
 alc = Dataset(
     name="alc",
     source="defra",
     versions=[
         Version(
             name="unknown",
-            path_read=(
-                "/dbfs/mnt/lab/unrestricted/elm_data/defra/alc/"
-                "ALC_BMV_UNION_Merge_3b.shp"
-            ),
+            path_read=("/dbfs/mnt/lab/unrestricted/elm_data/defra/alc/" "ALC_BMV_UNION_Merge_3b.shp"),
         ),
     ],
     keep_cols=["geometry", "ALC_GRADE"],
