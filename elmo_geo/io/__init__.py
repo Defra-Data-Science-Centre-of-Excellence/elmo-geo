ddbeba2b6beeed99d5a96eb542aa96290b7d9c4c
obi.thompsonsargoni@defra.gov.uk <obi.thompsonsargoni@defra.gov.uk>
2024-03-15 17:01:28 +0000

    Moving geometry.py location to resolve circular imports.

--- elmo_geo/io/__init__.py
+++ elmo_geo/io/__init__.py
@@ -1,3 +1,7 @@
 from .convert import to_gdf, to_sdf
+from .dash import ingest_dash
 from .download import download_link
+from .esri import ingest_esri
+from .file import to_gpq_partitioned as to_gpq
+from .osm import ingest_osm_pbf
 from .sentinel import download_sentinel
