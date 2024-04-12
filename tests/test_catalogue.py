ea8fb5d815c7f27d197c76fc131995f103afe675
aw-west-defra <aw-west-defra>
2024-04-12 16:05:09 +0000

    catalogue

---
+++ tests/test_catalogue.py
@@ -0,0 +1,19 @@
+from elmo_geo import register
+from elmo_geo.datasets.catalogue import load_catalogue
+from elmo_geo.datasets.datasets import datasets
+
+register()
+
+
+catalogue = load_catalogue()
+dataset_names = set(dataset.name for dataset in datasets)
+catalogue_names = set(dataset["name"].split("-")[1] for dataset in catalogue)
+
+
+def test_superset():
+    known_differences = {
+        "aonb",  # national_landscape
+        "lfa",  # moorline
+        "sssi",  # sssi_units
+    }
+    assert (catalogue_names.union(known_differences)).issuperset(dataset_names)
