827f9a249cc923e58cc57da5315a6bfe62eabd84
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2024-04-15 11:10:08 +0100

    added example

--- elmo_geo/utils/misc.py
+++ elmo_geo/utils/misc.py
@@ -33,17 +33,31 @@ def sh_run(exc: str, **kwargs):
 def info_sdf(sdf: SparkDataFrame, col: str = "geometry") -> SparkDataFrame:
     """Get Info about SedonaDataFrame
     Logs the number of partitions, geometry types, number of features, and average number of coordinates.
+
+    Example
+    ```py
+    info_sdf(sdf_water)
+    >>> INFO:elmo_geo.utils.log:partitions:  169
+    >>>                    gtype    count  mean_coords
+    >>> 0        ST_MultiPolygon    73914         94.9
+    >>> 1          ST_LineString  1049103         14.3
+    >>> 2  ST_GeometryCollection  1779376         94.2
+    >>> 3          ST_MultiPoint    21342          2.1
+    >>> 4     ST_MultiLineString  1593084         57.0
+    >>> 5               ST_Point   189777          1.0
+    >>> 6             ST_Polygon   737962         44.8
+    ```
     """
-    n = sdf.rdd.getNumParitions()
-    sdf = sdf.selectExpr(
-        f'ST_GeometryType({col) AS gtype',
-        f'ST_NPoints({col) AS n',
+    n = sdf.rdd.getNumPartitions()
+    df = sdf.selectExpr(
+        f'ST_GeometryType({col}) AS gtype',
+        f'ST_NPoints({col}) AS n',
     ).groupby("gtype").agg(
         F.count('gtype').alias('count'),
-        F.mean('n').alias('mean_coords'),
-    )
-    LOG.info(f"parts: {n}\ngtypes:  {sdf.rdd.collectAsMap()}")
-    return sdf
+        F.round(F.mean('n'), 1).alias('mean_coords'),
+    ).toPandas()
+    LOG.info(f"partitions:  {n}\n{df}")
+    return df
 
 
 def total_bounds(sdf: SparkDataFrame, col: str = "geometry"):
