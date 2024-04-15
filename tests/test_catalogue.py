17833569b1a2d2064711733678ad55085d43c71c
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2024-04-15 11:37:57 +0100

    Update test_catalogue.py

--- tests/test_catalogue.py
+++ tests/test_catalogue.py
@@ -1,7 +1,6 @@
 from elmo_geo.datasets.catalogue import load_catalogue
 from elmo_geo.datasets.datasets import datasets
 
-
 catalogue = load_catalogue()
 dataset_names = set(dataset.name for dataset in datasets)
 catalogue_names = set(dataset["name"].split("-")[1] for dataset in catalogue)
