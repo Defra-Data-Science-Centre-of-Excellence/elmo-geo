1f7823567febb0c3865793b8110dd864f1d9b293
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2024-04-17 12:07:23 +0000

    lint

--- tests/test_catalogue.py
+++ tests/test_catalogue.py
@@ -7,14 +7,14 @@ catalogue_names = set(dataset["name"].split("-")[1] for dataset in catalogue)
 
 
 def test_datasets_superset():
-    """Test all datasets are in the catalogue.json
-    """
+    """Test all datasets are in the catalogue.json"""
     known_differences = {
         "aonb",  # national_landscape
         "lfa",  # moorline
         "sssi",  # sssi_units
     }
     assert (catalogue_names.union(known_differences)).issuperset(dataset_names)
+
 
 def test_versions_superset():
     """Test all versions are in the catalogue.json
