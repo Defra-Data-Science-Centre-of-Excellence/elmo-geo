052be404cf2555a9b75bfd0bbac6ec8e63d3aa59
aw-west-defra <aw-west-defra>
2024-05-03 09:22:43 +0000

    ogr2ogr -"to_crs" "BNG"

--- notebooks/ingestion/02_convert.py
+++ notebooks/ingestion/02_convert.py
@@ -31,26 +31,27 @@ from elmo_geo.datasets.catalogue import run_task_on_catalogue, find_datasets
 from elmo_geo.io.file import to_parquet
 from elmo_geo.st.geometry import load_geometry
 from elmo_geo.st.index import sindex
-from elmo_geo.utils.misc import dbfs, sh_run, snake_case
+from elmo_geo.utils.misc import dbfs, snake_case
 from elmo_geo.utils.settings import BRONZE, SILVER
+
 
 register()
 
 # COMMAND ----------
 
-def list_layers(f):
+def list_layers(f: str) -> list[str]:
     try:
         layers = listlayers(f)
     except DriverError:
         layers = []
     return layers
 
-def list_files(f):
+def list_files(f: str) -> list[str]:
     for f1 in iglob(f+'**', recursive=True):
         if os.path.isfile(f1):
             yield f1
 
-def get_to_convert(f):
+def get_to_convert(f: str) -> list[tuple[str, str, str]]:
     if os.path.isfile(f):
         for layer in list_layers(f):
             name = f"layer={snake_case(layer)}"
@@ -65,22 +66,21 @@ def get_to_convert(f):
 def convert_file(f_in, f_out, layer):
     os.makedirs('/'.join(f_out.split('/')[:-1]), exist_ok=True)
     out = subprocess.run(f'''
-        DIR=/databricks/miniconda
-        export PROJ_LIB=$DIR/share/proj
-        $DIR/bin/ogr2ogr -f Parquet {f_out} {f_in} {layer}
+        export CONDA_DIR=/databricks/miniconda
+        export TMPDIR=/tmp
+        export OGR_GEOMETRY_ACCEPT_UNCLOSED_RING=NO
+        export PROJ_LIB=$CONDA_DIR/share/proj
+        $CONDA_DIR/bin/ogr2ogr -t_srs EPSG:27700 -f Parquet {f_out} {f_in} {layer}
     ''', capture_output=True, text=True, shell=True)
     LOG.info(out.__repr__())
     return out
 
 def convert(dataset):
     name = dataset['name']
-    crs = dataset.get('crs', 'EPSG:27700')  # Coordinate Reference System
-    precision = dataset.get('precision', 0.001)  # Precision at 1mm
-    resolution = dataset.get('resolution', 3)  # 3=1km BNG grid
     columns = dataset.get('columns', {})  # rename columns, but don't drop any
 
     f_raw = dataset.get('bronze', dataset['uri'])
-    f_tmp = f'/tmp/{name}.parquet'
+    f_tmp = f'/dbfs/tmp/{name}.parquet'
     f_out = f'{SILVER}/{name}.parquet'
 
     # Download
@@ -91,8 +91,8 @@ def convert(dataset):
     if f_raw.endswith(".parquet"):
         f_tmp = f_raw
     else:
-        for f0, name, layer in get_to_convert(f_raw):
-            f1 = f"{f_tmp}/{name}"
+        for f0, part, layer in get_to_convert(f_raw):
+            f1 = f"{f_tmp}/{part}"
             LOG.info(f"Converting: {f1}")
             convert_file(f0, f1, layer)
 
@@ -101,10 +101,11 @@ def convert(dataset):
     sdf = (spark.read.parquet(dbfs(f_tmp, True))
         .withColumn('fid', F.monotonically_increasing_id())
         .withColumnsRenamed(columns)
-        .withColumn('geometry', load_geometry(from_crs=crs))
+        .withColumn('geometry', load_geometry())
         .transform(sindex, method = "BNG", resolution = "10km", index_fn = "chipped_index")
         .transform(to_parquet, f_out)
     )
+    return sdf
 
     dataset['bronze'] = dataset['uri']
     dataset['silver'] = f_out
@@ -113,17 +114,33 @@ def convert(dataset):
 
 # COMMAND ----------
 
-# dataset_parcel = {
-#     'uri': '/dbfs/mnt/lab/restricted/ELM-Project/bronze/rpa-parcel-adas.parquet',
-#     'name': 'rpa-parcel-adas',
-#     "columns": {
-#         "Shape": "geometry"
-#     },
-#     'tasks': {
-#         'convert': 'todo'
-#     }
-# }
-# convert(dataset_parcel)
+dataset_parcel = {
+    'uri': '/dbfs/mnt/lab/restricted/ELM-Project/bronze/rpa-parcel-adas.parquet',
+    'name': 'rpa-parcel-adas',
+    "columns": {
+        "RLR_RW_REFERENCE_PARCELS_DEC_21_LPIS_REF": "id_parcel",
+        "Shape": "geometry"
+    },
+    'tasks': {
+        'convert': 'todo'
+    }
+}
+sdf = convert(dataset_parcel)
+sdf.display()
+
+# COMMAND ----------
+
+dataset_hedge = {
+    'uri': '/dbfs/mnt/lab/restricted/ELM-Project/bronze/rpa-hedge-adas.parquet',
+    'name': 'rpa-hedge-adas',
+    "columns": {
+        "geom": "geometry"
+    },
+    'tasks': {
+        'convert': 'todo'
+    }
+}
+convert(dataset_hedge)
 
 
 dataset_osm = find_datasets('osm')[0]
