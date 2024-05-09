31b87cecdb3fcdcf3bc31d718c608d478c9c93ed
burrowsej <burrowsej@gmail.com>
2024-05-09 11:55:56 +0100

    Add new dataset for overlap with any SSSI rather than having each one's name (#122)
    * Add new dataset for overlap with any SSSI rather than having each one's name
    
    * patch test fail (temp)
    
    * format code
    
    ---------
    
    Co-authored-by: burrowsej <burrowsej>

--- tests/test_catalogue.py
+++ tests/test_catalogue.py
@@ -12,6 +12,7 @@ def test_datasets_superset():
         "aonb",  # national_landscape
         "lfa",  # moorline
         "sssi",  # sssi_units
+        "sssi_mask",  # na
     }
     assert (catalogue_names.union(known_differences)).issuperset(dataset_names)
 
