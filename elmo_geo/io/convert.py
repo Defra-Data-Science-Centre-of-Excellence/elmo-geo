041be1376be885d18e71aa40943fb6ae3b720f10
aw-west-defra <aw-west-defra>
2024-05-14 14:33:58 +0000

    hasattr was wrong, replaced with "in"

--- elmo_geo/io/convert.py
+++ elmo_geo/io/convert.py
@@ -142,23 +142,16 @@ def convert(dataset):
     columns = dataset.get("columns", {})
     LOG.info(f"Converting: {name}")
 
-    if hasattr(dataset, "uri"):  # to remove with new catalogue
+    if "uri" in dataset:  # to remove with new catalogue
         dataset["bronze"] = dataset["uri"]
         dataset.pop("uri")
 
     f_raw = dataset["bronze"]
     f_tmp = f"/dbfs/tmp/{name}.parquet" if not f_raw.endswith(".parquet") else f_raw
-    f_out = f"{SILVER}/{name}.parquet"
+    f_out = dataset.get('silver', f"{SILVER}/{name}.parquet")  # for restricted data
 
-    # Download
-    if not f_raw.startswith("/dbfs/"):
-        raise TypeError(f"Expecting /dbfs/ dataset: {f_raw}")
-
-    # Convert
     if not os.path.exists(f_tmp):
         convert_dataset(f_raw, f_tmp)
-
-    # Partition
     if not os.path.exists(f_out):
         partition_geoparquet(f_tmp, f_out, columns)
 
