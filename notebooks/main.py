8c8de39dd3a207e76b90746eb460630ad2d47643
aw-west-defra <aw-west-defra>
2024-05-23 09:18:10 +0000

    moved Water to merge

--- notebooks/main.py
+++ notebooks/main.py
@@ -10,9 +10,9 @@ register()
 # COMMAND ----------
 
 dbutils.notebook.run("./ingestion/02_convert", 0)
-dbutils.notebook.run("./boundary/Water", 0)
+dbutils.notebook.run("./ingestion/merge_water", 0)
 dbutils.notebook.run("./ingestion/03_sjoin", 0)
 
 # COMMAND ----------
 
-# MAGIC %sh ls /dbfs/mnt/lab/restricted/ELM-Project/silver
+# MAGIC %sh ls /dbfs/mnt/lab/restricted/ELM-Project/silver/
