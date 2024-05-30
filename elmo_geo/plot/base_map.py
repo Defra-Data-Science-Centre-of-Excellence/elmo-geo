53820fe2ae701c3d225b612714cb3cab8c7e7952
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2024-05-30 09:36:45 +0100

    pull main -> aw-water
    * merge
    
    * distance 24 for water elig
    
    * make all polygons for easier union aggr
    
    * update water
    
    * lots of stuff
    
    * not needed
    
    * rename
    
    * added water dataset and id_parcel
    
    * added force run_task_on_catalogue
    
    * moved Water to merge
    
    * don't force by default
    
    * lookup_parcel check if exists
    
    * lookup parcel run and catalogue done
    
    * outer join OOMed
    
    * make fmt
    
    * small fixes
    
    * revert sjoin, moving to separate PR
    
    * osm tag analysis
    
    * make fmt
    
    * obi's comments
    
    ---------

--- elmo_geo/plot/base_map.py
+++ elmo_geo/plot/base_map.py
@@ -1,4 +1,21 @@
 import contextily as ctx
 import dotenv
+import matplotlib.pyplot as plt
 
-os_provider = ctx.providers.OrdnanceSurvey.Light(key=dotenv.get_key(".env", "OS_KEY"))
+from elmo_geo.utils.types import GeoDataFrame
+
+os_key = dotenv.get_key(".env", "OS_KEY")
+os_provider = ctx.providers.OrdnanceSurvey.Light(key=os_key)
+
+
+def plot_gdf(gdf: GeoDataFrame, figsize: tuple[float, float] = (9, 9), **kwargs):
+    """GeoDataFrame plot, but with an added contextily basemap"""
+    fig, ax = plt.subplots(figsize=figsize)
+    ax.axis("off")
+    gdf.plot(ax=ax, **kwargs)
+    ctx.add_basemap(
+        ax=ax,
+        crs=gdf.crs or "EPSG:27700",
+        source=os_provider if os_key else None,
+    )
+    return ax
---
+++ notebooks/plots/folium_view.py
@@ -0,0 +1,46 @@
+# Databricks notebook source
+import os
+
+import folium
+import numpy as np
+import pandas as pd
+from matplotlib import colormaps
+from matplotlib.colors import to_hex
+
+from elmo_geo.io import to_gdf
+from elmo_geo.io.download import download_link
+
+
+def get_n_colours(n: int, cmap="viridis") -> list[str]:
+    cmap = colormaps[cmap].resampled(n)
+    if hasattr(cmap, "colors"):
+        colours = cmap.colors
+    else:
+        colours = np.array([colour for colours in cmap._segmentdata.values() for colour in colours])
+    colours = np.apply_along_axis(to_hex, axis=1, arr=colours).tolist()
+    while len(colours) < n:
+        colours.extend(colours)
+    return colours[:n]
+
+
+# COMMAND ----------
+
+tile = "NU13"
+path = "/dbfs/mnt/lab/restricted/ELM-Project/silver"
+
+m, filepaths = folium.Map(), os.listdir(path)
+for filepath, colour in zip(filepaths, get_n_colours(len(filepaths), "hsv")):
+    f, name = f"{path}/{filepath}/sindex={tile}", filepath.replace(".parquet", "")
+    try:
+        df = pd.read_parquet(f).pipe(to_gdf)
+        df.explode(index_parts=True).explore(m=m, name=name, color=colour, show=False)
+        print("+", name, sep="\t")
+    except Exception as err:
+        print("-", name, err, sep="\t")
+folium.LayerControl().add_to(m)
+
+f = f"/dbfs/FileStore/sindex={tile}.html"
+m.save(f)
+download_link(f)
+
+m
