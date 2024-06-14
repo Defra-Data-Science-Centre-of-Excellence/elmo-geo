5fee8ef18ec6587d4ebde1b6caa26dffd8633ab4
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2024-06-14 13:23:29 +0000

    make fmt

--- notebooks/analysis/compare_overlaps.py
+++ notebooks/analysis/compare_overlaps.py
@@ -3,7 +3,8 @@
 # MAGIC # Compare Overlap Parcels methods
 # MAGIC Compare the old method process_datasets with the task overlap from 04_prop.
 # MAGIC
-# MAGIC SSSI has a very small absolute difference which means they're very similar, and the relative difference is small and negative imply a slight increase in capture with the new methodology.
+# MAGIC SSSI has a very small absolute difference which means they're very similar, and the relative difference is small and negative imply a slight increase in
+# MAGIC capture with the new methodology.
 # MAGIC Same with National Parks.  Most of the differences at very close to -1 or +1, which implies a difference in the parcels used.
 # MAGIC
 # MAGIC ### Conclusion: success
@@ -18,14 +19,14 @@ df_sssi = (
     pd.read_feather("/dbfs/mnt/lab/unrestricted/elm/elmo/sssi/sssi.feather")
     .merge(
         pd.read_parquet("/dbfs/mnt/lab/restricted/ELM-Project/silver/overlap-sssi_units-2024_03_07.parquet"),
-        on = "id_parcel",
-        how = "outer",
+        on="id_parcel",
+        how="outer",
     )
     .assign(
-        proportion = lambda df: df["proportion"].fillna(0),
-        proportion_0m = lambda df: df["proportion_0m"].fillna(0),
-        diff = lambda df: df["proportion"] - df["proportion_0m"],
-        abs_diff = lambda df: df["diff"].abs(),
+        proportion=lambda df: df["proportion"].fillna(0),
+        proportion_0m=lambda df: df["proportion_0m"].fillna(0),
+        diff=lambda df: df["proportion"] - df["proportion_0m"],
+        abs_diff=lambda df: df["diff"].abs(),
     )
 )
 
@@ -33,14 +34,14 @@ df_np = (
     pd.read_feather("/dbfs/mnt/lab/unrestricted/elm/elmo/national_park/national_park.feather")
     .merge(
         pd.read_parquet("/dbfs/mnt/lab/restricted/ELM-Project/silver/overlap-national_park-2024_01_30.parquet"),
-        on = "id_parcel",
-        how = "outer",
+        on="id_parcel",
+        how="outer",
     )
     .assign(
-        proportion = lambda df: df["proportion"].fillna(0),
-        proportion_0m = lambda df: df["proportion_0m"].fillna(0),
-        diff = lambda df: df["proportion"] - df["proportion_0m"],
-        abs_diff = lambda df: df["diff"].abs(),
+        proportion=lambda df: df["proportion"].fillna(0),
+        proportion_0m=lambda df: df["proportion_0m"].fillna(0),
+        diff=lambda df: df["proportion"] - df["proportion_0m"],
+        abs_diff=lambda df: df["diff"].abs(),
     )
 )
 
@@ -53,7 +54,3 @@ df_sssi.describe()
 
 df_np.hist("diff", bins=21)
 df_np.describe()
-
-# COMMAND ----------
-
-
