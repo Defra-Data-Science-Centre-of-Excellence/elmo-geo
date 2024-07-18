1b4b12bfdc55bfa7d0fc98d91478dc85cb4cd645
burrowsej <burrowsej@gmail.com>
2024-07-18 11:07:13 +0100

    Add Forestry Commission England Woodland Creation Sensitivity Maps (#176)
    * done
    
    * add catalog
    
    ---------
    
    Co-authored-by: burrowsej <burrowsej>

--- elmo_geo/datasets/__init__.py
+++ elmo_geo/datasets/__init__.py
@@ -5,10 +5,43 @@ from pathlib import Path
 
 from elmo_geo.utils.log import LOG
 
-from .fc_agroforestry import sfi_agroforestry, sfi_agroforestry_parcels, sfi_agroforestry_raw
+from .fc_woodland_sensitivity import (
+    sfi_agroforestry,
+    sfi_agroforestry_parcels,
+    sfi_agroforestry_raw,
+    woodland_creation_sensitivity,
+    woodland_creation_sensitivity_parcels,
+    woodland_creation_sensitivity_raw,
+    woodland_creation_sensitivity_var1,
+    woodland_creation_sensitivity_var1_parcels,
+    woodland_creation_sensitivity_var1_raw,
+    woodland_creation_sensitivity_var2,
+    woodland_creation_sensitivity_var2_parcels,
+    woodland_creation_sensitivity_var2_raw,
+    woodland_creation_sensitivity_var3,
+    woodland_creation_sensitivity_var3_parcels,
+    woodland_creation_sensitivity_var3_raw,
+)
 from .rpa_reference_parcels import reference_parcels
 
-catalogue = [reference_parcels, sfi_agroforestry_raw, sfi_agroforestry, sfi_agroforestry_parcels]
+catalogue = [
+    reference_parcels,
+    sfi_agroforestry_raw,
+    sfi_agroforestry,
+    sfi_agroforestry_parcels,
+    woodland_creation_sensitivity,
+    woodland_creation_sensitivity_parcels,
+    woodland_creation_sensitivity_raw,
+    woodland_creation_sensitivity_var1,
+    woodland_creation_sensitivity_var1_parcels,
+    woodland_creation_sensitivity_var1_raw,
+    woodland_creation_sensitivity_var2,
+    woodland_creation_sensitivity_var2_parcels,
+    woodland_creation_sensitivity_var2_raw,
+    woodland_creation_sensitivity_var3,
+    woodland_creation_sensitivity_var3_parcels,
+    woodland_creation_sensitivity_var3_raw,
+]
 """List of datasets in `elmo_geo`."""
 
 
