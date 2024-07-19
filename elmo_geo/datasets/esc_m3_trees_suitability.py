93195857c81aefc12d93496ff323c8022fa49a73
obi.thompsonsargoni@defra.gov.uk <obi.thompsonsargoni@defra.gov.uk>
2024-07-19 13:09:29 +0000

    Doncstring updates

--- elmo_geo/datasets/esc_m3_trees_suitability.py
+++ elmo_geo/datasets/esc_m3_trees_suitability.py
@@ -28,7 +28,7 @@ esc_suitability_broadleaved_raw = SourceDataset(
     is_geo=False,
     source_path="/dbfs/mnt/lab/unrestricted/elm_data/evast/M3_trees_rcp45/2024-07-04/EVAST_M3_native_broadleaved_rcp45.csv",
 )
-"""Definition for the raw sourced version of ESC broadleaved tree suitability dataset, recevied from EVAST."""
+"""Raw version of ESC broadleaved tree suitability dataset, recevied from EVAST."""
 
 esc_suitability_coniferous_raw = SourceDataset(
     name="esc_suitability_coniferous_raw",
@@ -38,7 +38,7 @@ esc_suitability_coniferous_raw = SourceDataset(
     is_geo=False,
     source_path="/dbfs/mnt/lab/unrestricted/elm_data/evast/M3_trees_rcp45/2024-07-04/EVAST_M3_productive_conifer_rcp45.csv",
 )
-"""Definition for the raw sourced version of ESC coniferous tree suitability dataset, recevied from EVAST."""
+"""Raw version of ESC coniferous tree suitability dataset, recevied from EVAST."""
 
 esc_suitability_riparian_raw = SourceDataset(
     name="esc_suitability_riparian_raw",
@@ -48,15 +48,67 @@ esc_suitability_riparian_raw = SourceDataset(
     is_geo=False,
     source_path="/dbfs/mnt/lab/unrestricted/elm_data/evast/M3_trees_rcp45/2024-07-04/EVAST_M3_riparian_rcp45.csv",
 )
-"""Definition for the raw sourced version of ESC riparian tree suitability dataset, recevied from EVAST."""
+"""Raw version of ESC riparian tree suitability dataset, recevied from EVAST."""
 
 class ESCTreeSuitabilityModel(DataFrameModel):
-    """Model describing the `esc_suitability_*` datasets.
+    """Model describing the derived esc tree suitability dataset.
+    Single row per parcel, with a suitability score and number of species count for each of the 
+    woodland types (broadleaved, coniferous, riparian) adn each of the modelled time periods
+    (which prefix the variables)
 
     Parameters:
-        id_parcel: 11 character RPA reference parcel ID (including the sheet ID) e.g. `SE12263419`.
-        mean_species_suitability: The mean tree suitability score for that parcels, averaged over tree species.
-        mean_species_suitability_quintile: The quintile of the mean tree suitability score.
+        id_parcel: 11 character RPA reference parcel ID (including the sheet ID) e.g. `SE12263419`.#
+        nopeatArea: Non peat area of parcel.
+        T1_2021_2028_T2_2021_2028_broadleaved_suitability: Broadleaved suitability score for the 
+            T1_2021_2028_T2_2021_2028 time periods
+        T1_2021_2028_T2_2021_2028_n_broadleaved_species: Broadleaved species count for the 
+            T1_2021_2028_T2_2021_2028 time periods
+        T1_2029_2036_T2_2021_2036_broadleaved_suitability: Broadleaved suitability score for the 
+            T1_2029_2036_T2_2021_2036 time periods
+        T1_2029_2036_T2_2021_2036_n_broadleaved_species: Broadleaved species count for the 
+            T1_2029_2036_T2_2021_2036 time periods
+        T1_2037_2050_T2_2021_2050_broadleaved_suitability: Broadleaved suitability score for the 
+            T1_2037_2050_T2_2021_2050 time periods
+        T1_2037_2050_T2_2021_2050_n_broadleaved_species: Broadleaved species count for the 
+            T1_2037_2050_T2_2021_2050 time periods
+        T1_2051_2100_T2_2021_2100_broadleaved_suitability: Broadleaved suitability score for the 
+            T1_2051_2100_T2_2021_2100 time periods
+        T1_2051_2100_T2_2021_2100_n_broadleaved_species: Broadleaved species count for the 
+            T1_2051_2100_T2_2021_2100 time periods
+
+        T1_2021_2028_T2_2021_2028_coniferous_suitability: Coniferous suitability score for the 
+            T1_2021_2028_T2_2021_2028 time periods
+        T1_2021_2028_T2_2021_2028_n_coniferous_species: Coniferous species count for the 
+            T1_2021_2028_T2_2021_2028 time periods
+        T1_2029_2036_T2_2021_2036_coniferous_suitability: Coniferous suitability score for the 
+            T1_2029_2036_T2_2021_2036 time periods
+        T1_2029_2036_T2_2021_2036_n_coniferous_species: Coniferous species count for the 
+            T1_2029_2036_T2_2021_2036 time periods
+        T1_2037_2050_T2_2021_2050_coniferous_suitability: Coniferous suitability score for the 
+            T1_2037_2050_T2_2021_2050 time periods
+        T1_2037_2050_T2_2021_2050_n_coniferous_species: Coniferous species count for the 
+            T1_2037_2050_T2_2021_2050 time periods
+        T1_2051_2100_T2_2021_2100_coniferous_suitability: Coniferous suitability score for the 
+            T1_2051_2100_T2_2021_2100 time periods
+        T1_2051_2100_T2_2021_2100_n_coniferous_species: Coniferous species count for the 
+            T1_2051_2100_T2_2021_2100 time periods
+
+        T1_2021_2028_T2_2021_2028_riparian_suitability: Riparian suitability score for the 
+            T1_2021_2028_T2_2021_2028 time periods
+        T1_2021_2028_T2_2021_2028_n_riparian_species: Riparian species count for the 
+            T1_2021_2028_T2_2021_2028 time periods
+        T1_2029_2036_T2_2021_2036_riparian_suitability: Riparian suitability score for the 
+            T1_2029_2036_T2_2021_2036 time periods
+        T1_2029_2036_T2_2021_2036_n_riparian_species: Riparian species count for the 
+            T1_2029_2036_T2_2021_2036 time periods
+        T1_2037_2050_T2_2021_2050_riparian_suitability: Riparian suitability score for the 
+            T1_2037_2050_T2_2021_2050 time periods
+        T1_2037_2050_T2_2021_2050_n_riparian_species: Riparian species count for the 
+            T1_2037_2050_T2_2021_2050 time periods
+        T1_2051_2100_T2_2021_2100_riparian_suitability: Riparian suitability score for the 
+            T1_2051_2100_T2_2021_2100 time periods
+        T1_2051_2100_T2_2021_2100_n_riparian_species: Riparian species count for the 
+            T1_2051_2100_T2_2021_2100 time periods
     """
     id_parcel: str
     nopeatArea: float = Field(nullable=False)
@@ -90,6 +142,10 @@ class ESCTreeSuitabilityModel(DataFrameModel):
 
 
 def _convert_to_long_format(sdf: SparkDataFrame) -> SparkDataFrame:
+    """Select the area, sutability, and yield_class variables for a single
+    species at a time and concatenate into a long format dataframe. Facilitates 
+    more convenient calculation of average suitability across species.
+    """
     vars_pattern = r"^(\w+)_(suitability|area|yield_class)$"
     vars_cols = [c for c in sdf.columns if re.match(vars_pattern, c) is not None]
     other_cols = [c for c in sdf.columns if c not in vars_cols]
@@ -112,6 +168,9 @@ def _convert_to_long_format(sdf: SparkDataFrame) -> SparkDataFrame:
     return sdf_long
 
 def _calculate_mean_suitability(sdf: SparkDataFrame, woodland_type: str) -> SparkDataFrame:
+    """Calculate mean suitability over species within a parcel and model time frame. 
+    Calculates weighted mean based on area.
+    """
     return (sdf.groupBy(["RLR_RW_REFERENCE_PARCELS_DEC_21_LPIS_REF", "period_AA_T1", "period_T2"])
  .agg(
     F.first("nopeatArea").alias("nopeatArea") ,
@@ -121,6 +180,9 @@ def _calculate_mean_suitability(sdf: SparkDataFrame, woodland_type: str) -> Spar
 )
     
 def _convert_to_wide_format(sdf: SparkDataFrame, woodland_type: str) -> SparkDataFrame:
+    """Convert dataframe to wide format so that each row is a singel parcel id. Pivots the
+    time period fields and concatenates these with variable column headings.
+    """
     return (sdf
             .withColumn("T1_T2", F.expr("CONCAT('T1_',period_AA_T1, '_T2_', period_T2 )"))
             .groupby("RLR_RW_REFERENCE_PARCELS_DEC_21_LPIS_REF", "nopeatArea")
@@ -132,6 +194,8 @@ def _convert_to_wide_format(sdf: SparkDataFrame, woodland_type: str) -> SparkDat
             )
 
 def _transform_single_woodland_type(sdf: SparkDataFrame, woodland_type:str) -> SparkDataFrame:
+    """Combine individual transformations that are applied to a single esc trees source dataframe.
+    """
     return (sdf
             .transform(_convert_to_long_format)
             .transform(_calculate_mean_suitability, woodland_type)
@@ -144,8 +208,8 @@ def _transform(esc_broadleaved: Dataset,
                esc_coniferous: Dataset,
                esc_riparian: Dataset,
                ) -> pd.DataFrame:
-    """Calculate the average suitability for each parcel across all tree species for the
-    broadleaved, coniferous, and riparian datasets."""
+    """Produce single dataset indicating parcel level suitability for broadleaved, coniferous, and riparian
+    woodland types for multiple modelled time periods. Returned dataset has a single row per parcel id."""
 
     # Coniferious dataset has incorrect values in the RC columns. Drop these
     # TODO: Update with corrected coniferous dataset
@@ -174,4 +238,5 @@ esc_tree_suitability = DerivedDataset(
     dependencies = [esc_suitability_broadleaved_raw, esc_suitability_coniferous_raw, esc_suitability_riparian_raw],
     model = ESCTreeSuitabilityModel,
 )
-"""Definition for the ESC tree suitability dataset aggregated to provide a single broadleaved tree suitability score per parcel."""
+"""Definition for the ESC tree suitability dataset aggregated to provide tree suitability scores 
+for broadleaved, coniferous, and riparian trees over different modelled time periods for each parcel."""
