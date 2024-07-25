7cae31610230e3db70103b4a517b72c03d8b3ef4
KaneCunliffe-DEFRA <KaneCunliffe-DEFRA>
2024-07-25 13:35:01 +0000

    error fix using make fmt in terminal

--- elmo_geo/datasets/__init__.py
+++ elmo_geo/datasets/__init__.py
@@ -28,9 +28,8 @@ from .fc_woodland_sensitivity import (
     woodland_creation_sensitivity_var3_parcels,
     woodland_creation_sensitivity_var3_raw,
 )
-from .rpa_reference_parcels import reference_parcels
-
 from .ons_itl import itl2_boundaries
+from .rpa_reference_parcels import reference_parcels
 
 catalogue = [
     itl2_boundaries,
