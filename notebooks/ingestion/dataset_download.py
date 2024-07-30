0ccca3ae2d062a0cc317b9d9c896c9185d7c387c
obi.thompsonsargoni@defra.gov.uk <obi.thompsonsargoni@defra.gov.uk>
2024-07-30 15:18:18 +0000

    Adding widgets for medallion and source

--- notebooks/ingestion/dataset_download.py
+++ notebooks/ingestion/dataset_download.py
@@ -17,13 +17,33 @@ from elmo_geo.io.download import download_link
 
 # COMMAND ----------
 
-names = sorted([f"{d.name}" for d in catalogue])
-default_name = names[0]
-dbutils.widgets.dropdown("dataset", default_name, names)
-name = dbutils.widgets.get("dataset")
-dataset = next(d for d in catalogue if d.name == name)
+medallions = sorted({f"{d.level0}" for d in catalogue})
+default_medallion = medallions[0]
+dbutils.widgets.dropdown("A - Medallion", default_medallion, medallions)
 
-[print(k, v, sep=":\t") for k, v in dataset.__dict__.items()]
+# COMMAND ----------
+
+medallion = dbutils.widgets.get("A - Medallion")
+sources = sorted({f"{d.level1}" for d in catalogue if d.level0==medallion})
+default_source = sources[0]
+dbutils.widgets.dropdown("B - Source", default_source, sources)
+
+# COMMAND ----------
+
+source = dbutils.widgets.get("B - Source")
+datasets = sorted({f"{d.name}" for d in catalogue if (d.level0==medallion) and (d.level1==source)})
+default_dataset = datasets[0]
+dbutils.widgets.dropdown("C - Dataset", default_dataset, datasets)
+
+# COMMAND ----------
+
+dataset = dbutils.widgets.get("C - Dataset")
+dataset = next(d for d in catalogue if d.name == dataset)
+[print(k, v, sep=":\t") for k, v in dataset.__dict__.items()];
+
+# COMMAND ----------
+
+dataset.path
 
 # COMMAND ----------
 
