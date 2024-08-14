9e87bd9efd7df9a2450df2d64ff258a6b7942e09
obi.thompsonsargoni@defra.gov.uk <obi.thompsonsargoni@defra.gov.uk>
2024-08-14 10:59:12 +0000

    Functions to test that combine sjoin and proportion calculation

--- elmo_geo/etl/transformations.py
+++ elmo_geo/etl/transformations.py
@@ -7,9 +7,9 @@ from pyspark.sql import functions as F
 
 from elmo_geo.st.geometry import load_geometry
 from elmo_geo.st.join import sjoin
+from elmo_geo.utils.types import SparkDataFrame
 
 from .etl import Dataset
-
 
 def _agg_calc_proportion(
     geometry_left: str = "geometry_left",
@@ -25,6 +25,31 @@ def _agg_calc_proportion(
     string = f"LEAST(GREATEST({string}, 0), 1)"
     return F.expr(f"{string} AS {column}")
 
+def sjoin_calc_proportion(df_parcels: SparkDataFrame, 
+                          df_features: SparkDataFrame, 
+                          columns: list[str] | None = None,
+                          )-> SparkDataFrame:
+        return (sjoin(df_parcels, df_features)
+                .groupby("id_parcel", *columns)
+                .agg(_agg_calc_proportion()))
+
+def sjoin_calc_proportion_dump(df_parcels: SparkDataFrame, 
+                          df_features: SparkDataFrame, 
+                          columns: list[str] | None = None,
+                          )-> SparkDataFrame:
+        return (sjoin(
+            df_parcels.withColumn("geometry", F.expr("EXPLODE(ST_Dump(geometry))")), 
+            df_features)
+                .groupby("id_parcel", *columns)
+                        .agg(
+            F.expr("ST_Union_Aggr(geometry_left) AS geometry_left"),
+            F.expr("ST_Union_Aggr(geometry_right) AS geometry_right"),
+        )
+        .withColumn("geometry_intersection", F.expr("ST_Intersection(geometry_left, geometry_right)"))
+        .withColumn("area_left", F.expr("ST_Area(geometry_left)"))
+        .withColumn("area_intersection", F.expr("ST_Area(geometry_intersection)"))
+        .withColumn("proportion", F.col("area_intersection") / F.col("area_left"))
+        .drop("area_left", "area_intersection", "geometry_left", "geometry_right", "geometry_intersection"))
 
 def join_parcels(
     parcels: Dataset,
@@ -63,9 +88,4 @@ def join_parcels(
         .withColumn("geometry", load_geometry(encoding_fn="", simplify_tolerence=simplify_tolerence))
         .withColumn("geometry", F.expr(f"ST_SubdivideExplode(geometry, {max_vertices})"))
     )
-    return (
-        sjoin(df_parcels, df_feature)
-        .groupby("id_parcel", *columns)
-        .agg(_agg_calc_proportion())
-        .toPandas()
-    )
+    return sjoin_calc_proportion(sdf_parcel, sdf_features, columns).toPandas()
