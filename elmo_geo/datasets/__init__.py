377bcd5320062a9272ad6160d6a5aabd32a7c90b
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2024-08-22 11:24:59 +0000

    ons datasets

--- elmo_geo/datasets/__init__.py
+++ elmo_geo/datasets/__init__.py
@@ -34,8 +34,22 @@ from .fc_woodland_sensitivity import (
 from .hedges import rpa_hedges_parcels, rpa_hedges_raw  # TODO: link, parcel_boundary_segments
 from .living_england import living_england_habitat_map_phase_4_parcel, living_england_habitat_map_phase_4_raw  # TODO: link
 from .moor import moorline_parcel, moorline_raw  # TODO: link, gpkg
-from .ons import *  # noqa TODO: links*2, do
-from .ons import itl2_boundaries_parcels, itl2_boundaries_raw
+from .ons import (  # TODO: links*2, itl3
+    bua_parcels,
+    bua_raw,
+    country_parcels,
+    country_raw,
+    cua_parcels,
+    cua_raw,
+    itl2_parcels,
+    itl2_raw,
+    lad_parcels,
+    lad_raw,
+    region_parcels,
+    region_raw,
+    ward_parcels,
+    ward_raw,
+)
 from .os import *  # noqa TODO: blocker geoparquet
 from .osm import osm_parcel, osm_raw, osm_tidy  # TODO: link, fn_osm_tidy
 from .peat import peaty_soils_parcels, peaty_soils_raw  # TODO: link
@@ -53,23 +67,35 @@ from .rpa_reference_parcels import reference_parcels  # TODO: link
 catalogue = [
     alc_parcels,
     alc_raw,
+    bua_parcels,
+    bua_raw,
     commons_parcels,
     commons_raw,
+    country_parcels,
+    country_raw,
+    cua_parcels,
+    cua_raw,
     flood_risk_areas_parcels,
     flood_risk_areas_raw,
     ewco_nature_recovery_priority_habitat,
     ewco_nature_recovery_priority_habitat_parcels,
     ewco_nature_recovery_priority_habitat_raw,
-    itl2_boundaries_raw,
-    itl2_boundaries_parcels,
+    itl2_parcels,
+    itl2_raw,
     living_england_habitat_map_phase_4_parcel,
     living_england_habitat_map_phase_4_raw,
+    lad_parcels,
+    lad_raw,
     moorline_parcel,
     moorline_raw,
     reference_parcels,
+    region_parcels,
+    region_raw,
     sfi_agroforestry,
     sfi_agroforestry_parcels,
     sfi_agroforestry_raw,
+    ward_parcels,
+    ward_raw,
     woodland_creation_sensitivity,
     woodland_creation_sensitivity_parcels,
     woodland_creation_sensitivity_raw,
--- elmo_geo/datasets/ons.py
+++ elmo_geo/datasets/ons.py
@@ -1,25 +1,31 @@
 """Office of National Statistics (ONS) Open GeoPortal Layers, provided by DASH.
 
-This is a collection of official subdivisions of England, these are typically used to limit analysis to domains of interest or identify regional bias.
+This is a collection of official subdivisions of England[^ONS Geographies], these are typically used to limit analysis to domains of interest or identify
+regional bias.
 
-| Dataset Name | Short Name | Domain | Similar | Example |
+| Dataset Name | Short Name | Domain | Similar | Example/Notes |
 | ------------ | ---------- | ------ | ------- | ------- |
-| [International Territorial Level 1][^itl1] | ITL1 | UK | Regions |
-| [International Territorial Level 2][^itl2] | ITL2 | UK |
-| [International Territorial Level 3][^itl2] | ITL3 | UK |
 | [Countries][^country] | | UK | | England, Northern Ireland, Scotland, Wales |
+| [International Territorial Level 1][^itl1] | ITL1/Regions | UK | "Regions"[^ONS Geographies] | |
+| [International Territorial Level 2][^itl2] | ITL2 | UK | "Counties and groups of counties"[^ONS Geographies] | |
+| [International Territorial Level 3][^itl2] | ITL3 | UK | "Counties and groups of unitary authorities"[^ONS Geographies] | Not available on DASH. |
 | [Councils][^council] | | UK |
 | [Counties and Unitary Authorities][^cau] | CAU | England |
 | [Local Authority Districts][^lad] | LAD | England |
 | [Wards][^ward] | | England |
 | [Parishes][^parish] | | England |
 
-/dbfs/mnt/base/unrestricted/source_ons_open_geography_portal/dataset_countries_bdy_uk_bfe/format_GEOPARQUET_countries_bdy_uk_bfe/LATEST_countries_bdy_uk_bfe/
-/dbfs/mnt/base/unrestricted/source_ons_open_geography_portal/dataset_regions_bdy_en_bfe/format_GEOPARQUET_regions_bdy_en_bfe/LATEST_regions_bdy_en_bfe/
-/dbfs/mnt/base/unrestricted/source_ons_open_geography_portal/dataset_counties_uni_authorities_bdy_uk_bfe/format_GEOPARQUET_counties_uni_authorities_bdy_uk_bfe/LATEST_counties_uni_authorities_bdy_uk_bfe/
-/dbfs/mnt/base/unrestricted/source_ons_open_geography_portal/dataset_local_authority_districts_bdy_uk_bfe/format_GEOPARQUET_local_authority_districts_bdy_uk_bfe/LATEST_local_authority_districts_bdy_uk_bfe/
-/dbfs/mnt/base/unrestricted/source_ons_open_geography_portal/dataset_wards_bdy_uk_bfe/format_GEOPARQUET_wards_bdy_uk_bfe/LATEST_wards_bdy_uk_bfe/
+| Resolution | Code | Description |
+| ---------- | ---- | ----------- |
+| Full Extent | BFE | Full resolution boundaries go to the Extent of the Realm (Low Water Mark) and are the most detailed of the boundaries. |
+| Full Clipped | BFC | Full resolution boundaries that are clipped to the coastline (Mean High Water mark). |
+| Generalised Clipped | BGC | Generalised to 20m and clipped to the coastline (Mean High Water mark) and more generalised than the BFE boundaries. |
+| Super Generalised Clipped | BSC | Generalised to 200m and clipped to the coastline (Mean High Water mark). |
+| Ultra Generalised Clipped | BUC | Generalised to 500m and clipped to the coastline (Mean High Water mark). |
+| Grid, Extent | BGE | Grid formed of equally sized cells which extend beyond the coastline. |
+| Generalised, Grid | BGG | Generalised 50m grid squares. |
 
+[^ONS Geographies]: https://www.ons.gov.uk/methodology/geography/ukgeographies/
 [^itl1]:
 [^itl2]: https://geoportal.statistics.gov.uk/datasets/6750ae0351c749c4b40b31e5740233a0_0/explore?location=54.959130%2C-3.316600%2C6.04
 [^itl3]:
@@ -42,52 +48,369 @@ from elmo_geo.etl.transformations import join_parcels
 from .rpa_reference_parcels import reference_parcels
 
 
-class ITL2BoundariesRaw(DataFrameModel):
-    """Model for ONS ITL2 (counties and groups of counties) dataset.
+# Countries
+class CountryRaw(DataFrameModel):
+    """Model for ONS Countries dataset.
 
     Parameters:
-        ITL221CD: Reference unique id for each geographic area ie TLC1.
-        ITL221NM: Name of the county or group of counties ie Tees Valley and Durham
-        geometry: The ITL geospatial polygons are in EPSG:27700.
+        fid: Is the country code, from 2023.
+        name: Is the country name, from 2023.
+        geometry: 4 MultiPolygons in EPSG:27700 at BFE.
     """
 
-    ITL221CD: str = Field(coerce=True)
-    ITL221NM: str = Field(coerce=True)
+    fid: str = Field(coerce=True, unique=True, alias="CTRY23CD")
+    name: str = Field(coerce=True, alias="CTRY23NM")
     geometry: Geometry(crs=SRID) = Field(coerce=True)
 
 
-itl2_boundaries_raw = SourceDataset(
-    name="itl2_boundaries_raw",
+country_raw = SourceDataset(
+    name="country_raw",
     level0="bronze",
     level1="ons",
-    model=ITL2BoundariesRaw,
+    model=CountryRaw,
+    restricted=False,
+    source_path="/dbfs/mnt/base/unrestricted/source_ons_open_geography_portal/dataset_countries_bdy_uk_bfe/format_GEOPARQUET_countries_bdy_uk_bfe/LATEST_countries_bdy_uk_bfe/",
+)
+
+
+class CountryParcels(DataFrameModel):
+    """Model for ONS Counties with parcel dataset.
+
+    Parameters:
+        id_parcel: 11 character RPA reference parcel ID (including the sheet ID) e.g. `SE12263419`.
+        fid: Is the country code, from 2023.
+        name: Is the country name, from 2023.
+        proportion: The proportion of the parcel that intersects with feature.
+    """
+
+    id_parcel: str = Field()
+    fid: str = Field()
+    name: str = Field()
+    proportion: float = Field(ge=0, le=1)
+
+
+country_parcels = DerivedDataset(
+    name="country_parcels",
+    level0="silver",
+    level1="ons",
+    restricted=False,
+    func=partial(join_parcels, columns=["fid", "name"]),
+    dependencies=[reference_parcels, country_raw],
+    model=CountryParcels,
+)
+
+
+# ITL1/Regions
+class RegionRaw(DataFrameModel):
+    """Model for ONS Regions dataset.
+
+    Parameters:
+        fid: Is the region code, from 2023.
+        name: Is the region name, from 2023.
+        geometry: 9 (Multi)Polygons in EPSG:27700 at BFE.
+    """
+
+    fid: str = Field(coerce=True, unique=True, alias="RGN23CD")
+    name: str = Field(coerce=True, alias="RGN23NM")
+    geometry: Geometry(crs=SRID) = Field(coerce=True)
+
+
+region_raw = SourceDataset(
+    name="region_raw",
+    level0="bronze",
+    level1="ons",
+    model=RegionRaw,
+    restricted=False,
+    source_path="/dbfs/mnt/base/unrestricted/source_ons_open_geography_portal/dataset_regions_bdy_en_bfe/format_GEOPARQUET_regions_bdy_en_bfe/LATEST_regions_bdy_en_bfe/",
+)
+
+
+class RegionParcels(DataFrameModel):
+    """Model for ONS Regions with parcel dataset.
+
+    Parameters:
+        id_parcel: 11 character RPA reference parcel ID (including the sheet ID) e.g. `SE12263419`.
+        fid: Is the region code, from 2023.
+        name: Is the region name, from 2023.
+        proportion: The proportion of the parcel that intersects with feature.
+    """
+
+    id_parcel: str = Field()
+    fid: str = Field()
+    name: str = Field()
+    proportion: float = Field(ge=0, le=1)
+
+
+region_parcels = DerivedDataset(
+    name="region_parcels",
+    level0="silver",
+    level1="ons",
+    restricted=False,
+    func=partial(join_parcels, columns=["fid", "name"]),
+    dependencies=[reference_parcels, region_raw],
+    model=RegionParcels,
+)
+
+
+# ITL2
+class ITL2Raw(DataFrameModel):
+    """Model for ONS ITL2 (counties and groups of counties) dataset.
+
+    Parameters:
+        fid: Reference unique id for each geographic area ie TLC1, from 2021.
+        name: Name of the county or group of counties ie Tees Valley and Durham, from 2021.
+        geometry: MultiPolygons in EPSG:27700 at BGC.
+    """
+
+    fid: str = Field(coerce=True, unique=True, alias="ITL221CD")
+    name: str = Field(coerce=True, alias="ITL221NM")
+    geometry: Geometry(crs=SRID) = Field(coerce=True)
+
+
+itl2_raw = SourceDataset(
+    name="itl2_raw",
+    level0="bronze",
+    level1="ons",
+    model=ITL2Raw,
     restricted=False,
     source_path="/dbfs/mnt/base/unrestricted/source_ons_open_geography_portal/dataset_int_territorial_lvl2_2021_uk_bgc_v2/format_GPKG_int_territorial_lvl2_2021_uk_bgc_v2/LATEST_int_territorial_lvl2_2021_uk_bgc_v2/International_Territorial_Level_2_January_2021_UK_BGC_V2_2022_1205324512979248673.gpkg",
 )
 
 
-class ITL2BoundariesParcels(DataFrameModel):
+class ITL2Parcels(DataFrameModel):
     """Model for ONS ITL2 with parcel dataset.
 
     Parameters:
         id_parcel: 11 character RPA reference parcel ID (including the sheet ID) e.g. `SE12263419`.
-        ITL221CD: Reference unique id for each geographic area ie TLC1.
-        ITL221NM: Name of the county or group of counties ie Tees Valley and Durham
+        fid: Reference unique id for each geographic area ie TLC1.
+        name: Name of the county or group of counties ie Tees Valley and Durham
         proportion: The proportion of the parcel that intersects with the itl2 boundary.
     """
 
     id_parcel: str = Field()
-    ITL221CD: str = Field()
-    ITL221NM: str = Field()
+    fid: str = Field()
+    name: str = Field()
     proportion: float = Field(ge=0, le=1)
 
 
-itl2_boundaries_parcels = DerivedDataset(
-    name="itl2_boundaries_parcels",
+itl2_parcels = DerivedDataset(
+    name="itl2_parcels",
     level0="silver",
     level1="ons",
     restricted=False,
-    func=partial(join_parcels, columns=["ITL221NM", "ITL221CD"]),
-    dependencies=[reference_parcels, itl2_boundaries_raw],
-    model=ITL2BoundariesParcels,
+    func=partial(join_parcels, columns=["fid", "name"]),
+    dependencies=[reference_parcels, itl2_raw],
+    model=ITL2Parcels,
+)
+
+
+# ITL3
+# TODO
+
+
+# Counties and Unitary Authorities (CAUs)
+class CUARaw(DataFrameModel):
+    """Model for ONS Counties and Unitary Authorities (CUA) dataset.
+
+    Parameters:
+        fid: Is the CUA code, from 2023.
+        name: Is the CUA name, from 2023.
+        geometry: (Multi)Polygons in EPSG:27700 at BFE.
+    """
+
+    fid: str = Field(coerce=True, unique=True, alias="CTYUA23CD")
+    name: str = Field(coerce=True, alias="CTYUA23NM")
+    geometry: Geometry(crs=SRID) = Field(coerce=True)
+
+
+cua_raw = SourceDataset(
+    name="cua_raw",
+    level0="bronze",
+    level1="ons",
+    model=CUARaw,
+    restricted=False,
+    source_path="/dbfs/mnt/base/unrestricted/source_ons_open_geography_portal/dataset_counties_uni_authorities_bdy_uk_bfe/format_GEOPARQUET_counties_uni_authorities_bdy_uk_bfe/LATEST_counties_uni_authorities_bdy_uk_bfe/",
+)
+
+
+class CUAParcels(DataFrameModel):
+    """Model for ONS CUA with parcel dataset.
+
+    Parameters:
+        id_parcel: 11 character RPA reference parcel ID (including the sheet ID) e.g. `SE12263419`.
+        fid: Is the CUA code, from 2023.
+        name: Is the CUA name, from 2023.
+        proportion: The proportion of the parcel that intersects with feature.
+    """
+
+    id_parcel: str = Field()
+    fid: str = Field()
+    name: str = Field()
+    proportion: float = Field(ge=0, le=1)
+
+
+cua_parcels = DerivedDataset(
+    name="cua_parcels",
+    level0="silver",
+    level1="ons",
+    restricted=False,
+    func=partial(join_parcels, columns=["fid", "name"]),
+    dependencies=[reference_parcels, cua_raw],
+    model=CUAParcels,
+)
+
+
+# Local Authority Districts (LADs)
+class LADRaw(DataFrameModel):
+    """Model for ONS Local Authority Districts (LADs) dataset.
+
+    Parameters:
+        fid: Is the LAD code, from 2023.
+        name: Is the LAD name, from 2023.
+        geometry: (Multi)Polygons in EPSG:27700 at BFE.
+    """
+
+    fid: str = Field(coerce=True, unique=True, alias="LAD23CD")
+    name: str = Field(coerce=True, alias="LAD23NM")
+    geometry: Geometry(crs=SRID) = Field(coerce=True)
+
+
+lad_raw = SourceDataset(
+    name="lad_raw",
+    level0="bronze",
+    level1="ons",
+    model=LADRaw,
+    restricted=False,
+    source_path="/dbfs/mnt/base/unrestricted/source_ons_open_geography_portal/dataset_local_authority_districts_bdy_uk_bfe/format_GEOPARQUET_local_authority_districts_bdy_uk_bfe/LATEST_local_authority_districts_bdy_uk_bfe/",
+)
+
+
+class LADParcels(DataFrameModel):
+    """Model for ONS LAD with parcel dataset.
+
+    Parameters:
+        id_parcel: 11 character RPA reference parcel ID (including the sheet ID) e.g. `SE12263419`.
+        fid: Is the LAD code, from 2023.
+        name: Is the LAD name, from 2023.
+        proportion: The proportion of the parcel that intersects with feature.
+    """
+
+    id_parcel: str = Field()
+    fid: str = Field()
+    name: str = Field()
+    proportion: float = Field(ge=0, le=1)
+
+
+lad_parcels = DerivedDataset(
+    name="lad_parcels",
+    level0="silver",
+    level1="ons",
+    restricted=False,
+    func=partial(join_parcels, columns=["fid", "name"]),
+    dependencies=[reference_parcels, lad_raw],
+    model=LADParcels,
+)
+
+
+# Wards
+class WardRaw(DataFrameModel):
+    """Model for ONS Wards dataset.
+
+    Parameters:
+        fid: Is the Ward code, from 2023.
+        name: Is the Ward name, from 2023.
+        geometry: (Multi)Polygons in EPSG:27700 at BFE.
+    """
+
+    fid: str = Field(coerce=True, unique=True, alias="WD23CD")
+    name: str = Field(coerce=True, alias="WD23NM")
+    geometry: Geometry(crs=SRID) = Field(coerce=True)
+
+
+ward_raw = SourceDataset(
+    name="ward_raw",
+    level0="bronze",
+    level1="ons",
+    model=WardRaw,
+    restricted=False,
+    source_path="/dbfs/mnt/base/unrestricted/source_ons_open_geography_portal/dataset_wards_bdy_uk_bfe/format_GEOPARQUET_wards_bdy_uk_bfe/LATEST_wards_bdy_uk_bfe/",
+)
+
+
+class WardParcels(DataFrameModel):
+    """Model for ONS Wards with parcel dataset.
+
+    Parameters:
+        id_parcel: 11 character RPA reference parcel ID (including the sheet ID) e.g. `SE12263419`.
+        fid: Is the Ward code, from 2023.
+        name: Is the Ward name, from 2023.
+        proportion: The proportion of the parcel that intersects with feature.
+    """
+
+    id_parcel: str = Field()
+    fid: str = Field()
+    name: str = Field()
+    proportion: float = Field(ge=0, le=1)
+
+
+ward_parcels = DerivedDataset(
+    name="ward_parcels",
+    level0="silver",
+    level1="ons",
+    restricted=False,
+    func=partial(join_parcels, columns=["fid", "name"]),
+    dependencies=[reference_parcels, ward_raw],
+    model=WardParcels,
+)
+
+
+# Built Up Areas (BUA)
+class BUARaw(DataFrameModel):
+    """Model for ONS Built Up Areas (BUA) dataset.
+
+    Parameters:
+        fid: Is the BUA code, from 2022.
+        name: Is the BUA name, from 2022.
+        geometry: (Multi)Polygons in EPSG:27700 at BGG.
+    """
+
+    fid: str = Field(coerce=True, unique=True, alias="BUA22CD")
+    name: str = Field(coerce=True, alias="BUA22NM")
+    geometry: Geometry(crs=SRID) = Field(coerce=True)
+
+
+bua_raw = SourceDataset(
+    name="bua_raw",
+    level0="bronze",
+    level1="ons",
+    model=BUARaw,
+    restricted=False,
+    source_path="/dbfs/mnt/base/unrestricted/source_ons_open_geography_portal/dataset_built_up_areas_2022_gb_bgg/format_GEOPARQUET_built_up_areas_2022_gb_bgg/LATEST_built_up_areas_2022_gb_bgg",
+)
+
+
+class BUAParcels(DataFrameModel):
+    """Model for ONS Built Up Areas (BUA) with parcel dataset.
+
+    Parameters:
+        id_parcel: 11 character RPA reference parcel ID (including the sheet ID) e.g. `SE12263419`.
+        fid: Is the BUA code, from 2022.
+        name: Is the BUA name, from 2022.
+        proportion: The proportion of the parcel that intersects with feature.
+    """
+
+    id_parcel: str = Field()
+    fid: str = Field()
+    name: str = Field()
+    proportion: float = Field(ge=0, le=1)
+
+
+bua_parcels = DerivedDataset(
+    name="bua_parcels",
+    level0="silver",
+    level1="ons",
+    restricted=False,
+    func=partial(join_parcels, columns=["fid", "name"]),
+    dependencies=[reference_parcels, bua_raw],
+    model=BUAParcels,
 )
