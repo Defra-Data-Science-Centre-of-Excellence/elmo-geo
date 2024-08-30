922fc65c20e4eba04c5292714a61329064961677
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2024-08-30 09:16:31 +0000

    better doc

--- elmo_geo/io/file.py
+++ elmo_geo/io/file.py
@@ -4,6 +4,8 @@ from pathlib import Path
 import geopandas as gpd
 import pyarrow.parquet as pq
 from geopandas.io.arrow import SUPPORTED_VERSIONS, _geopandas_to_arrow
+
+# from pyarrow.dataset import write_dataset
 from pyspark.sql import functions as F
 
 from elmo_geo.utils.log import LOG
@@ -44,19 +46,22 @@ def gdf_to_partitioned_parquet(
 
 
 def to_parquet(df: DataFrame, path: str, partition_cols: str | None = None):
-    """Write any DataFrame to parquet, partition if needed.
+    """Write a DataFrame to parquet and partition.
+    Takes in Spark, Pandas, or GeoPandas dataframe, remove any already written data, and writes a new dataframe.
 
     Parameters:
-        df
-        f
-        partition_cols
+        df: Dataframe to be written as parquet, (geo)parquet, or parquet+wkb (for spark).
+        path: Output path to write the data into.
+        partition_cols: Column to partition the dataframe by, if no column is provided the dataframe with not be partitioned (unless spark sees it necessary).
     """
     if Path(path).exists():
         LOG.warning("Replacing Dataset")
         shutil.rmtree(path)
     if isinstance(df, SparkDataFrame):
-        df.withColumn("geometry", F.expr("ST_AsBinary(geometry)")).write.parquet(dbfs(path, True), partitionBy=partition_cols, mode="overwrite")
+        df.withColumn("geometry", F.expr("ST_AsBinary(geometry)")).write.parquet(dbfs(path, True), partitionBy=partition_cols)
     elif isinstance(df, GeoDataFrame):
+        # table = _geopandas_to_arrow(df)
+        # write_dataset(table, path, format="parquet", partition_cols=partition_cols)
         gdf_to_partitioned_parquet(df, path, partition_cols=partition_cols)
     elif isinstance(df, PandasDataFrame):
         df.to_parquet(path, partition_cols=partition_cols)
