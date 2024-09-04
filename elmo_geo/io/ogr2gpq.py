9fb881526f4a37f51eab1f553572b00cc5e7bd53
aw-west-defra <aw-west-defra>
2024-09-04 15:27:08 +0000

    simplify ogr2gpq

--- elmo_geo/io/ogr2gpq.py
+++ elmo_geo/io/ogr2gpq.py
@@ -18,48 +18,26 @@ def list_layers(f: str) -> list[str]:
     return layers
 
 
-def list_files(f: str) -> list[str]:
-    """List all the files in a directory
-    similar to os.walk, but yielding full paths"""
-    for f1 in iglob(f + "**", recursive=True):
-        if os.path.isfile(f1):
-            yield f1
+def ogr_to_geoparquet(f_in: str, f_out: str):
+    """Convert a folder or glob path of vector files and all their layers into a parquet dataset.
 
-
-def get_to_convert(f: str) -> list[tuple[str, str, str]]:
-    """Get all the ogr readable files and their layers in a folder"""
-    if os.path.isfile(f):
-        for layer in list_layers(f):
-            name = f"layer={snake_case(layer)}"
-            yield f, name, layer
-    else:
-        f = f if f.endswith("/") else f + "/"
-        for f1 in list_files(f):
-            for layer in list_layers(f1):
-                name = f"file={snake_case(f1.replace(f, '').split('.')[0])}/layer={snake_case(layer)}"
-                yield f1, name, layer
-
-
-def ogr_to_geoparquet(f_in: str, f_out: str, layer: str):
-    """Convert a vector file's layer into a (Geo)Parquet file using gdal>3.5 ogr2ogr"""
-    os.makedirs("/".join(f_out.split("/")[:-1]), exist_ok=True)
-    out = subprocess.run(
-        f"""
-        export CONDA_DIR=/databricks/miniconda
-        export TMPDIR=/tmp
-        export OGR_GEOMETRY_ACCEPT_UNCLOSED_RING=NO
-        export PROJ_LIB=$CONDA_DIR/share/proj
-        $CONDA_DIR/bin/ogr2ogr -t_srs EPSG:27700 -f Parquet {f_out} {f_in} {layer}
-    """,
-        capture_output=True,
-        text=True,
-        shell=True,
-    )
-    LOG.info(out.__repr__())
-
-
-def convert_dataset(f_in: str, f_out: str):
-    """Convert a folder of vector files and all their layers into a parquet dataset"""
-    for f0, part, layer in get_to_convert(f_in):
-        f1 = f"{f_out}/{part}"
-        ogr_to_geoparquet(f0, f1, layer)
+    BUG: multiple layers with the same name will be overwritten.
+    Intentional: mergeSchema is required for datasets with different schemas.
+    """
+    for f0 in iglob(f_in + "**", recursive=True):
+        if os.path.isfile(f0):
+            for layer in list_layers(f0):
+                f1 = f"{f_out}/layer={snake_case(layer)}/"
+                os.makedirs(f1, exist_ok=True)
+                f1 += "part-0.snappy.parquet"
+                LOG.info(f"ogr2ogr: {f1}")
+                out = subprocess.run(
+                    f"""
+                    export PATH=/databricks/miniconda/bin:$PATH
+                    ogr2ogr -t_srs 'EPSG:27700' -f Parquet '{f1}' '{f0}' '{layer}'
+                """,
+                    capture_output=True,
+                    text=True,
+                    shell=True,
+                )
+                LOG.debug(out.__repr__())
