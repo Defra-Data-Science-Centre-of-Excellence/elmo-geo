d15d3dfb4bc052b53842f3c32922b7b819678ab4
obi.thompsonsargoni@defra.gov.uk <obi.thompsonsargoni@defra.gov.uk>
2024-09-11 10:47:57 +0000

    Drafting new dataset module for habitat creation classification

---
+++ elmo_geo/datasets/fcp_habitat_classification.py
@@ -0,0 +1,10 @@
+"""Parcel level habitat creation and management classifications.
+
+Habitat creation ELM actions require specifying which habitats would most suitable be created
+on a parcel. This is done by combining multiple data sources:
+- Soilscapes linked to which habitats can be created on each soil type
+- Proximity to priority habitats
+- Habitat map habitat type
+
+Joining these three datasets together. 
+"""
