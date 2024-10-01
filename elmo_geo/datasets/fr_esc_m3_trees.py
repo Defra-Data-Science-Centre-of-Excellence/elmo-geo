630bdafd09064e4026ca2e06351e521bf0bbfc64
obi.thompsonsargoni@defra.gov.uk <obi.thompsonsargoni@defra.gov.uk>
2024-10-01 15:40:47 +0000

    Adding dataset of no-peat parcel 1km proportion.

--- elmo_geo/datasets/fr_esc_m3_trees.py
+++ elmo_geo/datasets/fr_esc_m3_trees.py
@@ -41,14 +41,21 @@ sequestration and positive values net emissions.
 """
 
 
+import geopandas as gpd
 import pyspark.sql.functions as F
 from pandera import DataFrameModel, Field
 from pandera.engines.geopandas_engine import Geometry
+from pyspark.sql.types import BinaryType, StringType, StructField, StructType
 
-from elmo_geo.etl import DerivedDataset, SourceGlobDataset
+from elmo_geo.etl import Dataset, DerivedDataset, SourceGlobDataset
+from elmo_geo.etl.transformations import sjoin_parcel_proportion, sjoin_parcels
+from elmo_geo.st.geometry import load_geometry
 from elmo_geo.st.join import sjoin
+from elmo_geo.utils.types import PandasDataFrame
 
 from .os import os_bng_raw
+from .peat import peaty_soils_raw
+from .rpa_reference_parcels import reference_parcels
 
 
 class ESCM3WoodlandScenariosRaw(DataFrameModel):
@@ -221,4 +228,91 @@ esc_m3_geo = DerivedDataset(
     partition_cols=["woodland_type", "rcp", "period_T2"],
 )
 """ESC M3 Trees model outputs joined to 1km OS BNG tile geometries.
+"""
+
+
+def _sjoin_bng_to_no_peat_parcel(
+    reference_parcels: Dataset,
+    peaty_soils_raw: Dataset,
+    os_bng_raw: Dataset,
+) -> PandasDataFrame:
+    """Produces dataframe linking parcels to 1km BNG tiles.
+
+    Combines parcels, peat and OS BNG geometries to identify the proportion of non-peat parcel area
+    intersected by different 1km BNG grid tiles.
+
+    These proportions are used to aggregate ESC M3 outputs to parcel level, since the ESC-CARBINE model
+    outputs results for 1km grid tiles.
+    """
+
+    schema = StructType([StructField("id_parcel", StringType(), True), StructField("geometry", BinaryType(), True)])
+
+    def _udf_difference(pdfs):
+        """Get the parcel geometry excluding peaty soils"""
+        for pdf in pdfs:
+            yield (
+                pdf.assign(geometry=gpd.GeoSeries.from_wkb(pdf["geometry_left"]).difference(gpd.GeoSeries.from_wkb(pdf["geometry_right"])).to_wkb()).reindex(
+                    columns=["id_parcel", "geometry"]
+                )
+            )
+
+    # parcels intersecting peat
+    sdf_peat = (
+        reference_parcels.sdf()
+        .select("id_parcel", "geometry")
+        .transform(sjoin_parcels, peaty_soils_raw.sdf().withColumn("geometry", load_geometry(encoding_fn="")))
+        .selectExpr(
+            "id_parcel",
+            "ST_AsBinary(geometry_left) as geometry_left",
+            "ST_AsBinary(geometry_right) as geometry_right",
+        )
+        .mapInPandas(_udf_difference, schema=schema)
+        .withColumn("geometry", load_geometry())
+        .withColumn("nopeat_area", F.expr("ST_Area(geometry)/10000"))
+    )
+
+    # all parcels
+    sdf_other = (
+        reference_parcels.sdf()
+        .join(sdf_peat.select("id_parcel", "nopeat_area"), on="id_parcel", how="left")
+        .filter("nopeat_area IS NULL")
+        .selectExpr("id_parcel", "geometry", "area_ha as nopeat_area")
+    )
+
+    return sjoin_parcel_proportion(sdf_other.unionByName(sdf_peat), os_bng_raw.sdf().filter("layer='1km_grid'"), columns=["tile_name", "nopeat_area"])
+
+
+class NoPeatParcelBNGModel(DataFrameModel):
+    """Proportion of no-peat parcel geometries intersected by BNG 1km grids.
+
+    Attributes:
+        id_parcel: Parcel ID
+        nopeat_area: Geographic area of parcel excluding intersecting peaty soils geometries.
+        tile_name: Name of 1km OS BNG tile intersected the parcel.
+        proportion: Proportion of parcel no peat area intersected by 1km BNG tile.
+    """
+
+    id_parcel: str = Field()
+    nopeat_area: float = Field()
+    tile_name: str = Field()
+    proportion: float = Field(ge=0, le=1)
+
+
+os_bng_no_peat_parcels = DerivedDataset(
+    name="os_bng_no_peat_parcels",
+    level0="silver",
+    level1="os",
+    restricted=False,
+    is_geo=False,
+    dependencies=[
+        reference_parcels,
+        peaty_soils_raw,
+        os_bng_raw,
+    ],
+    func=_sjoin_bng_to_no_peat_parcel,
+    model=NoPeatParcelBNGModel,
+)
+"""Proportion of non-peat parcels intersected by BNG 1km tiles.
+
+Used as an input to the ESC M3 data aggregation to parcels process.
 """
