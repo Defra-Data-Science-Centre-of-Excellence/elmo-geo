80bb905b20165c4d2f3cf8fa1c2a4ead6e61bd83
KaneCunliffe-DEFRA <KaneCunliffe-DEFRA>
2024-10-02 13:35:13 +0000

    updated source dataset model for tree detection

--- elmo_geo/datasets/fcp_tree_detection.py
+++ elmo_geo/datasets/fcp_tree_detection.py
@@ -5,54 +5,53 @@ in the notebooks/sylvan/Tree Features notebook, which makes use of functions in 
 notebooks/sylvan/tree_features.py file.
 """
 
-from functools import partial
 
 from pandera import DataFrameModel, Field
-from pandera.engines.geopandas_engine import Geometry
 
-from elmo_geo.etl import SRID, DerivedDataset, SourceDataset
-from elmo_geo.etl.transformations import sjoin_parcel_proportion
-
-from .rpa_reference_parcels import reference_parcels
+from elmo_geo.etl import SourceDataset
 
 
 #tree detections
 class TreeDetectionsRaw(DataFrameModel):
-      """Model for fcp tree detection datset.
+    """Model for fcp tree detection datset.
 
-    Parameters:
+    Attributes:
         SHEET_ID: Is the parcel sheet ID.
         PARCEL_ID: Is the parcel parcel ID.
         Perimeter_length: The length of the parcel perimeter. Calculated by getting the length of the boundary of the parcel polygon.
         SHEET_PARCEL_ID: Unique identifier for each parcel. Not duplicated, Produced by concatenating REF_PARCEL_SHEET_ID and REF_PARCEL_PARCEL_ID.
         hrtrees_count2: Number of hedgerow trees associated with this parcel.Given by intersecting 2m buffered hedgerow geometries with tree crown coordinates.
-        wbtrees_count2: 
-        wbtrees_count4:
-        perim_trees_count2:
-        crown_perim_length2:
-        int_trees_count2:
-        perim_trees_count4:
-        crown_perim_length4:
-        int_trees_count4:
-
+        wbtrees_count2: Number of water body trees associated with this parcel.
+                        Water bodies are filtered to exclude geometries tagged as ‘Sea’. The amount water bodies are buffered by 2m.
+        wbtrees_count4: Number of water body trees associated with this parcel.
+                        Water bodies are filtered to exclude geometries tagged as ‘Sea’. The amount water bodies are buffered by 4m.
+        perim_trees_count2: Number of trees that intersect with the parcel’s perimeter. The distance the parcel perimeter is buffered by 2m.
+        crown_perim_length2: The length of the parcel perimeter that intersects with perimeter tree crowns.
+                            Calculated by intersecting the crown geometries (polygons) of perimeter trees
+                            (trees whose crown coordinate intersects with the 2m buffered perimeter) with the parcel perimeter.
+        int_trees_count2: Number of trees in the parcel interior.
+                        The parcel interior is given by the difference between the 2m buffered parcel perimeter and the parcel geometry.
+        perim_trees_count4: Number of trees that intersect with the parcel’s perimeter. The distance the parcel perimeter is buffered by 4m.
+        crown_perim_length4: The length of the parcel perimeter that intersects with perimeter tree crowns.
+                            Calculated by intersecting the crown geometries (polygons) of perimeter trees
+        (trees whose crown coordinate intersects with the 4m buffered perimeter) with the parcel perimeter.
+        int_trees_count4: Number of trees in the parcel interior.
+                        The parcel interior is given by the difference between the 4m buffered parcel perimeter and the parcel geometry.
     """
 
+    SHEET_ID: str = Field()
     PARCEL_ID: str = Field()
-    Perimeter_length:
-    SHEET_PARCEL_ID:
-    hrtrees_count2:
-    wbtrees_count2: 
-    wbtrees_count4:
-    perim_trees_count2:
-    crown_perim_length2:
-    int_trees_count2:
-    perim_trees_count4:
-    crown_perim_length4:
-    int_trees_count4:
-
-    fid: str = Field(unique=True, alias="CTRY23CD")
-    name: str = Field(alias="CTRY23NM")
-    geometry: Geometry(crs=SRID) = Field()
+    Perimeter_length: float = Field()
+    SHEET_PARCEL_ID: str = Field(unique=True)
+    hrtrees_count2: int = Field()
+    wbtrees_count2: int = Field()
+    wbtrees_count4: int = Field()
+    perim_trees_count2: int = Field()
+    crown_perim_length2: float = Field()
+    int_trees_count2: int = Field()
+    perim_trees_count4: int = Field()
+    crown_perim_length4: float = Field()
+    int_trees_count4: int = Field()
 
 
     fcp_tree_detection_raw = SourceDataset(
