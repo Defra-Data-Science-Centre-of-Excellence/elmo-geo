864c9116679fe617049f1aac263b7123ec8ee0a7
obi.thompsonsargoni@defra.gov.uk <obi.thompsonsargoni@defra.gov.uk>
2024-10-17 10:11:33 +0000

    Reverting habitat changes.

--- elmo_geo/datasets/defra_priority_habitats.py
+++ elmo_geo/datasets/defra_priority_habitats.py
@@ -7,6 +7,7 @@ The data is not a exhaustive survey of habitat locations but is the best availab
 """
 from functools import partial, reduce
 
+import pandas as pd
 from pandera import DataFrameModel, Field
 from pandera.engines.geopandas_engine import Geometry
 from pyspark.sql import functions as F
@@ -14,10 +15,12 @@ from pyspark.sql import functions as F
 from elmo_geo.etl import SRID, Dataset, DerivedDataset, SourceDataset
 from elmo_geo.etl.transformations import sjoin_parcel_proportion
 from elmo_geo.st.geometry import load_geometry
-from elmo_geo.st.join import sjoin
+from elmo_geo.st.join import knn, sjoin
 from elmo_geo.utils.types import SparkDataFrame
 
 from .rpa_reference_parcels import reference_parcels
+
+DISTANCE_THRESHOLD = 5_000
 
 
 def split_mainhabs(sdf: SparkDataFrame) -> SparkDataFrame:
@@ -27,6 +30,60 @@ def split_mainhabs(sdf: SparkDataFrame) -> SparkDataFrame:
     and drops the original column.
     """
     return sdf.withColumn("habitat_name", F.expr("EXPLODE(SPLIT(mainhabs, ','))")).drop("mainhabs")
+
+
+_join_parcels = partial(sjoin_parcel_proportion, columns=["habitat_name"], fn_pre=split_mainhabs)
+
+
+def _habitat_proximity(parcels: Dataset, habitats: Dataset, habitat_filter_expr: str, max_vertices: int = 256) -> pd.DataFrame:
+    """Calculate the distance from each parcel to each selected habitat type.
+
+    Used to find the closest habitat to each parcel and the associated distance. Applies filter
+    to habitats dataset so that resulting proximity dataset is for a subset of priority habitats.
+
+    Imposes distance threshold, preset to 5km, to avoid costly distance calculations.
+
+    Parameters:
+        parcels: The parcels dataset.
+        habitats: The habitats dataset.
+        habitat_filter_exr: SQL expression for filtering the habitats dataset.
+        max_vertices: Max number of habitat geometries vertices. Geometries exceeding this threshold are split.
+
+    Returns:
+        DataFrame of parcel id to nearest habitat and the associated distance.
+    """
+    sdf_habitat = (
+        habitats.sdf()
+        .transform(split_mainhabs)
+        .filter(F.expr(habitat_filter_expr))
+        .select("habitat_name", "geometry")
+        .withColumn("geometry", F.expr(f"ST_SubdivideExplode(geometry, {max_vertices})"))
+    )
+
+    sdf_parcels = parcels.sdf().select("id_parcel", "geometry")
+
+    sdf_knn = knn(sdf_parcels, sdf_habitat, id_left="id_parcel", id_right="habitat_name", k=1, distance_threshold=DISTANCE_THRESHOLD).drop("rank")
+
+    # Aggregate to return dataset with single row per parcel, closest habitat and corresponding distance.
+    return sdf_knn.join(sdf_knn.groupBy("id_parcel").agg(F.min("distance").alias("distance")), on=["id_parcel", "distance"], how="inner").toPandas()
+
+
+_heathland_habitat_proximity = partial(_habitat_proximity, habitat_filter_expr="habitat_name like '%heath%'")
+_grassland_habitat_proximity = partial(
+    _habitat_proximity,
+    habitat_filter_expr="habitat_name in ('{}')".format(
+        "','".join(
+            [
+                "Lowland calcareous grassland",
+                "Upland calcareous grassland",
+                "Lowland dry acid grassland",
+                "Lowland meadows",
+                "Purple moor grass and rush pastures",
+                "Grass moorland",
+            ]
+        )
+    ),
+)
 
 
 class PHIEnglandRawModel(DataFrameModel):
@@ -45,7 +102,7 @@ class PHIEnglandRawModel(DataFrameModel):
     habcodes: str = Field()
     areaha: float = Field()
     version: str = Field()
-    fid: str = Field(unique=False, alias="uid")
+    fid: str = Field(unique=True, alias="uid")
     geometry: Geometry(crs=SRID) = Field()
 
 
@@ -54,17 +111,28 @@ class PriorityHabitatParcels(DataFrameModel):
 
     Parameters:
         id_parcel: 11 character RPA reference parcel ID (including the sheet ID) e.g. `SE12263419`.
-        fid: Unique identifier for habitat geometry. Because each geometry can be labelled with multiple
-            habitats the fid is retained in the aprcels join to ensure correct aggregation of proportions
-            in derived datasets.
         habitat_name: The name of the priority habitat.
         proportion: The proportion of the parcel that intersects with the spatial priority.
     """
 
     id_parcel: str = Field()
-    fid: str = Field()
     habitat_name: str = Field()
     proportion: float = Field(ge=0, le=1)
+
+
+class PriorityHabitatProximity(DataFrameModel):
+    """Model describing the distance between habitats in the
+    Defra Priority Habitats dataset and RPA parcels.
+
+    Parameters:
+        id_parcel: 11 character RPA reference parcel ID (including the sheet ID) e.g. `SE12263419`.
+        habitat_name: The name of the priority habitat.
+        distance: The distance from the parcel to this type of habitat.
+    """
+
+    id_parcel: str = Field()
+    habitat_name: str = Field()
+    distance: int = Field()
 
 
 defra_priority_habitat_england_raw = SourceDataset(
@@ -72,8 +140,6 @@ defra_priority_habitat_england_raw = SourceDataset(
     level0="bronze",
     level1="defra",
     restricted=False,
-    clean_geometry=True,  # Splits geometries resulting in duplciate fids.
-    partition_cols=["habcodes"],
     source_path="/dbfs/mnt/base/unrestricted/source_defra_data_services_platform/dataset_priority_habitats_inventory_eng/format_GEOPARQUET_priority_habitats_inventory_eng/LATEST_priority_habitats_inventory_eng/ne_priority_habitat_inventory_england.parquet",
     model=PHIEnglandRawModel,
 )
@@ -85,11 +151,46 @@ defra_priority_habitat_parcels = DerivedDataset(
     level1="defra",
     restricted=False,
     is_geo=False,
-    func=partial(sjoin_parcel_proportion, columns=["fid", "habitat_name"], fn_pre=split_mainhabs),
+    func=_join_parcels,
     dependencies=[reference_parcels, defra_priority_habitat_england_raw],
     model=PriorityHabitatParcels,
 )
 """Definition for Defra Priority Habitats joined to RPA Parcels."""
+
+
+defra_heathland_proximity_parcels = DerivedDataset(
+    name="defra_heathland_proximity_parcels",
+    level0="silver",
+    level1="defra",
+    restricted=False,
+    is_geo=False,
+    func=_heathland_habitat_proximity,
+    dependencies=[reference_parcels, defra_priority_habitat_england_raw],
+    model=PriorityHabitatProximity,
+)
+"""Defra Priority Habitats distance from parcels to heathland habitats.
+"""
+
+defra_grassland_proximity_parcels = DerivedDataset(
+    name="defra_grassland_proximity_parcels",
+    level0="silver",
+    level1="defra",
+    restricted=False,
+    is_geo=False,
+    func=_grassland_habitat_proximity,
+    dependencies=[reference_parcels, defra_priority_habitat_england_raw],
+    model=PriorityHabitatProximity,
+)
+"""Defra Priority Habitats distance from parcels to grassland habitats.
+
+The following grassland habitats are included:
+- Lowland calcareous grassland
+- Upland calcareous grassland
+- Lowland dry acid grassland
+- Lowland meadows
+- Purple moor grass and rush pastures
+- Grass moorland
+"""
 
 
 def _habitat_area_within_distance(
