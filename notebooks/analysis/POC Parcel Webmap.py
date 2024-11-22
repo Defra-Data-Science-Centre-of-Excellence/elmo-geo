15eeda7a0dce8a8c23eb4b866365c0baa9eb64fb
obi-defra <133129211+obi-defra@users.noreply.github.com>
2024-11-22 12:24:29 +0000

    fmt

--- notebooks/analysis/POC Parcel Webmap.py
+++ notebooks/analysis/POC Parcel Webmap.py
@@ -1,6 +1,6 @@
 # Databricks notebook source
 # MAGIC %sh
-# MAGIC pip install geojson jenkspy
+# MAGIC pip install jenkspy
 
 # COMMAND ----------
 
@@ -16,12 +16,14 @@
 # MAGIC
 # MAGIC ## Summarising things I have checks/explored
 # MAGIC - Parcel simplification: tollerance=100 is too high, parcels becomes messy and overlapping. tollerance=50 is roughly the upper limit.
-# MAGIC - Trying to display all parcels in a folium map resulted in this error in Databricks 'Command result size exceeds limit: Exceeded 20971520 bytes (current = 20977394)'. Almost certain SCE machines also wouldn't be able to handle this.
+# MAGIC - Trying to display all parcels in a folium map resulted in this error in Databricks 'Command result size
+# MAGIC   exceeds limit: Exceeded 20971520 bytes (current = 20977394)'. Almost certain SCE machines also wouldn't be able to handle this.
 # MAGIC - Means that we need a way to displays sections of the country at a time as people scroll/move around the map.
 # MAGIC
 # MAGIC ## Conclusion
 # MAGIC
-# MAGIC It will be challenging to flexibly visualise parcel level data for the whole country. To view parcels we would likely need to restrict the map to a certain area of `<`100km2
+# MAGIC It will be challenging to flexibly visualise parcel level data for the whole country.
+# MAGIC To view parcels we would likely need to restrict the map to a certain area of `<`100km2.
 # MAGIC
 # MAGIC ## Useful links for mapping with folium and leaflet
 # MAGIC
@@ -31,46 +33,37 @@
 
 # COMMAND ----------
 
-import os, shutil
-import re
-from datetime import datetime as dt
-import pyspark.sql.functions as F
-import pandas as pd
-import time
-import geojson
 import json
-import folium
-import numpy as np
 from functools import partial
+
+import folium
 import geopandas as gpd
-
+import numpy as np
+import pandas as pd
+import pyspark.sql.functions as F
 from folium import Map
-from geojson.feature import Feature, FeatureCollection
 from folium.plugins import FeatureGroupSubGroup
-from branca.element import MacroElement, Element
-
-from elmo_geo.utils.register import register
-from elmo_geo.io.convert import to_gdf
-from elmo_geo.st.udf import st_clean
-from elmo_geo.io.download import download_link
-
-from elmo_geo.utils.types import SparkDataFrame
 
 from elmo_geo.datasets import (
-    reference_parcels,
     os_bng_parcels,
+    reference_parcels,
 )
+from elmo_geo.io.download import download_link
+from elmo_geo.st.udf import st_clean
+from elmo_geo.utils.register import register
+from elmo_geo.utils.types import SparkDataFrame
 
 register(dir="elmo-geo2")
 
 # COMMAND ----------
+
 
 def base_empty_map(centre=None, zoom_start=None):
     """Prepares a folium map centered in a central GPS point of Toulouse"""
     if centre is None:
         centre = (52.133236898161755, -1.2970901724513781)
     if zoom_start is None:
-        zoom_start=7
+        zoom_start = 7
     m = Map(
         location=centre,
         zoom_start=zoom_start,
@@ -81,6 +74,7 @@ def base_empty_map(centre=None, zoom_start=None):
                       CartoDB</a>""",
     )
     return m
+
 
 def foilium_map(geo_data, df_data, variable, variable_name, title="", nan_value=None, centre=None, zoom_start=None, **kwargs):
     m = base_empty_map(centre, zoom_start)
@@ -121,35 +115,40 @@ def foilium_map(geo_data, df_data, variable, variable_name, title="", nan_value=
 
     return m, cp
 
+
 # COMMAND ----------
 
 from_wkb = partial(gpd.GeoSeries.from_wkb, crs=27700)
 
+
 def st_to_json(sdf: SparkDataFrame, key: str = "tile_name", col: str = "geometry") -> SparkDataFrame:
-    """Convert a geographic spark dataframe to geojson, grouped by keys.
-    """
+    """Convert a geographic spark dataframe to geojson, grouped by keys."""
+
     def _fn(pdf):
         gdf = gpd.GeoDataFrame(pdf, geometry=from_wkb(pdf[col]), crs="epsg:27700").to_crs("epsg:4326")
-        return pd.DataFrame({key:[gdf[key].values[0]], "geojson":[json.dumps(gdf.to_json())]})
+        return pd.DataFrame({key: [gdf[key].values[0]], "geojson": [json.dumps(gdf.to_json())]})
 
     _sdf = sdf.withColumn(col, F.expr(f"ST_AsBinary({col})"))
     return _sdf.groupby(key).applyInPandas(_fn, f"{key} String, geojson String")
 
 
 # create geo-data
-def _transform_to_simplified_json(reference_parcels, tollerance=50, os_grid = "10km_grid"):
-    return (reference_parcels.sdf()
-       .transform(st_clean, tollerance=tollerance)
-       .join(os_bng_parcels.sdf().filter(f"layer='{os_grid}'"), on="id_parcel", how="left")
-       .orderBy("proportion", ascending=False)
-       .groupby("id_parcel").agg(
-           F.expr("FIRST(geometry) as geometry"),
-           F.expr("RAND() as profit"),
-           F.expr("FIRST(tile_name) as tile_name"),
-       )
-       .transform(st_to_json, key="tile_name")
-       .toPandas()
-       )
+def _transform_to_simplified_json(reference_parcels, tollerance=50, os_grid="10km_grid"):
+    return (
+        reference_parcels.sdf()
+        .transform(st_clean, tollerance=tollerance)
+        .join(os_bng_parcels.sdf().filter(f"layer='{os_grid}'"), on="id_parcel", how="left")
+        .orderBy("proportion", ascending=False)
+        .groupby("id_parcel")
+        .agg(
+            F.expr("FIRST(geometry) as geometry"),
+            F.expr("RAND() as profit"),
+            F.expr("FIRST(tile_name) as tile_name"),
+        )
+        .transform(st_to_json, key="tile_name")
+        .toPandas()
+    )
+
 
 pdf_json = _transform_to_simplified_json(reference_parcels)
 geo_data = json.loads(pdf_json.set_index("tile_name").iloc[0]["geojson"])
@@ -161,11 +160,11 @@ m = base_empty_map(None, None)
 # Add a main feature group for the base layers
 main_feature_group = folium.FeatureGroup(name="Main Layers", show=False).add_to(m)
 
-n=50
-for tile_name, geojson in pdf_json.head(n).values:
+n = 50
+for tile_name, geojson_string in pdf_json.head(n).values:
     layer_group = FeatureGroupSubGroup(main_feature_group, tile_name)
     folium.GeoJson(
-        json.loads(geojson),
+        json.loads(geojson_string),
         name=tile_name,
         style_function=lambda feature: {
             "fillColor": "#ffff00",
@@ -173,7 +172,7 @@ for tile_name, geojson in pdf_json.head(n).values:
             "weight": 2,
             "dashArray": "5, 5",
         },
-        show=True
+        show=True,
     ).add_to(layer_group)
 
     m.add_child(layer_group)
@@ -216,18 +215,16 @@ folium.LayerControl(collapsed=False).add_to(m)
 #         return self._code
 
 # Inject the JavaScript into the map
-#m.get_root().html.add_child(CustomJavaScript(js_code))
+# m.get_root().html.add_child(CustomJavaScript(js_code))
 
 # Save the map
-f_out = "/dbfs/FileStore/elmo-geo-downloads/poc_parcel_map.html" 
+f_out = "/dbfs/FileStore/elmo-geo-downloads/poc_parcel_map.html"
 m.save(f_out)
-download_link(f_out) # 34Mb htlm file
+download_link(f_out)  # 34Mb htlm file
 
 # COMMAND ----------
 
-df_eligibility = (reference_parcels.sdf()
-                  .selectExpr("id_parcel", "RAND() as eligible_proportion")
-                  .toPandas())
+df_eligibility = reference_parcels.sdf().selectExpr("id_parcel", "RAND() as eligible_proportion").toPandas()
 
 # Takes very long time - don't think this si the right way to do a choropleth for parcels. Need to pre calculate the colours.
 # variable = "eligible_proportion"
