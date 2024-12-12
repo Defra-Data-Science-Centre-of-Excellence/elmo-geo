eddd8157815b4a9144c475fd5344eeea80f6c3bc
obi.thompsonsargoni@defra.gov.uk <obi.thompsonsargoni@defra.gov.uk>
2024-12-12 16:56:59 +0000

    Drafting derived tree detection datasets

--- elmo_geo/datasets/fcp_tree_detection.py
+++ elmo_geo/datasets/fcp_tree_detection.py
@@ -1,7 +1,26 @@
+"""Engalnd Tree map produced from tree dectation classification of LiDAR Vegetation Object Model data.
+
+The outputs fromt he tree detection process are available as a source dataset. Derived datasets join these
+tree detections to parcels and parcel boundaries.
+
+A quality assurance review of the tree detection dataset is available in share point
+([Evidence_QA_template Tree Detection](https://defra.sharepoint.com/:x:/r/teams/Team1645/Evidence%20and%20Analysis%20WS/4.9_Workstream_Areas_Modelling_Strategy/4.9.7%20Modelling%20Strategy%20Documentation/Sylvan_Features/Lidar%20Tree%20Detection/Evidence_QA_template%20Tree%20Detection.xlsm?d=w04f7b4159fbd4517b89dcc949b122cf0&csf=1&web=1&e=7jP6hB))
+
+The [tree-detection](https://github.com/Defra-Data-Science-Centre-of-Excellence/tree-detection) GitHub repository
+contains the code used to produce the tree detections.
+"""
+from functools import partial
+
+import pyspark.sql.functions as F
 from pandera import DataFrameModel, Field
 from pandera.dtypes import Float64
 
-from elmo_geo.etl import SourceDataset
+from elmo_geo.etl import Dataset, DerivedDataset, SourceDataset
+from elmo_geo.etl.transformations import pivot_wide_sdf, sjoin_boundaries, sjoin_boundary_count
+from elmo_geo.utils.types import SparkDataFrame
+
+from .boundary import boundary_segments
+from .rpa_reference_parcels import reference_parcels
 
 
 # Tree Detctions Dataset - potentially temporay data used for a specific BTO request
@@ -38,9 +57,96 @@ fcp_tree_detection_raw = SourceDataset(
     is_geo=False,
     source_path="/dbfs/mnt/lab/unrestricted/elm/elmo/tree_features/tree_detections/tree_detections_202311231323.parquet/",
 )
-"""The 'tree_features' dataset is parcel level counts of the number of trees
-within a parcel and intersecting the perimeter of a parcel.
-It was created by DSMT FCP in the ots-sylvan-tree-features' branch of elmo-geo,
-in the notebooks/sylvan/Tree Features notebook, which makes use of functions in the
-notebooks/sylvan/tree_features.py file
-"""
+"""LiDAR derived tree map of England. Prodives estimated coordinate of peak of the tree crown and polygon of tree crown.
+It was created by DSMT FCP with the [tree-detection](https://github.com/Defra-Data-Science-Centre-of-Excellence/tree-detection)
+GitHub repository."""
+
+
+class FCPTBoundaryTreeCounts(DataFrameModel):
+    """Model for counts of trees intersecting parcel boudaries.
+    Attributes:
+        id_parcel: parcel id in which that boundary came from.
+        id_boundary: boundary id, unique id for each parcel boudnary segment.
+        m: length of the boundary geometry.
+        count_*m: Number of trees intersectin the boundary segment buffered at "*"
+    """
+
+    id_boundary: int = Field()
+    id_parcel: str = Field()
+    m: float = Field()
+    count_2m: float = Field(ge=0, le=1)
+    count_8m: float = Field(ge=0, le=1)
+    count_12m: float = Field(ge=0, le=1)
+    count_24m: float = Field(ge=0, le=1)
+
+
+def prep_tree_point(sdf):
+    return sdf.selectExpr("ST_GeomFromWKT(top_point) AS geometry")
+
+
+boundary_tree_count = DerivedDataset(
+    level0="silver",
+    level1="fcp",
+    name="boundary_tree_count",
+    model=FCPTBoundaryTreeCounts,
+    restricted=False,
+    func=partial(sjoin_boundary_count, fn_pre=prep_tree_point),
+    dependencies=[reference_parcels, boundary_segments, fcp_tree_detection_raw],
+    is_geo=False,
+)
+"""Counts fof trees intersecting parcel boundary segments."""
+
+
+class FCPTInteriorTreeCounts(DataFrameModel):
+    """Model for counts of trees intersecting parcel boudaries.
+    Attributes:
+        id_parcel: parcel id in which that boundary came from.
+        m: length of the boundary geometry.
+        count_*m: Number of trees intersectin the boundary segment buffered at "*"
+    """
+
+    id_parcel: str = Field()
+    m: float = Field()
+    count_0m: float = Field(ge=0, le=1)
+    count_2m: float = Field(ge=0, le=1)
+    count_8m: float = Field(ge=0, le=1)
+    count_12m: float = Field(ge=0, le=1)
+    count_24m: float = Field(ge=0, le=1)
+
+
+def sjoin_interior_count(
+    parcels: Dataset | SparkDataFrame,
+    boundary_segments: Dataset | SparkDataFrame,
+    features: Dataset | SparkDataFrame,
+    buffers: list[float] = [0, 2, 4, 8, 12, 24],
+    **kwargs,
+):
+    """Counts the number of trees in a parcel's interior.
+
+    Parcel interios defined by the difference between buffered boundary segment geometries
+    and the parcel geometry.
+    """
+    expr = "ST_Difference(ST_Union_Aggr(geometry), geoemtry_left)"
+    expr = f"ST_Intersection({expr}, geometry_right)"
+    expr = f"COUNT({expr}) as count"
+    return (
+        sjoin_boundaries(parcels, boundary_segments, features, distance=max(buffers), **kwargs)
+        .withColumn("geometry_right", F.expr("EXPLODE(ST_Dump(geometry_right))"))
+        .groupby("id_parcel", "buffer")
+        .agg(F.expr(expr))
+        .transform(pivot_wide_sdf, name_col="buffer", value_col="count")
+        .withColumnsRenamed({str(b): f"count_{b}m" for b in buffers})
+    )
+
+
+interior_tree_count = DerivedDataset(
+    level0="silver",
+    level1="fcp",
+    name="interior_tree_count",
+    model=FCPTInteriorTreeCounts,
+    restricted=False,
+    func=partial(sjoin_interior_count, fn_pre=prep_tree_point),
+    dependencies=[reference_parcels, boundary_segments, fcp_tree_detection_raw],
+    is_geo=False,
+)
+"""Counts of trees itersecting parcel interiors."""
