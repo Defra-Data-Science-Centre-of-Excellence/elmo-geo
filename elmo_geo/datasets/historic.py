81b26be69d3517a79ba38b8b0820660561c223a6
lijun-wei-defra <lijun-wei-defra>
2025-01-14 15:31:44 +0000

    Updated based on comments

--- elmo_geo/datasets/historic.py
+++ elmo_geo/datasets/historic.py
@@ -15,12 +15,9 @@ from functools import partial
 
 from pandera import DataFrameModel, Field
 from pandera.engines.geopandas_engine import Geometry
-from pyspark.sql import functions as F
 
-from elmo_geo.etl import SRID, Dataset, DerivedDataset, SourceDataset
+from elmo_geo.etl import SRID, DerivedDataset, SourceDataset
 from elmo_geo.etl.transformations import combine_long
-from elmo_geo.st.udf import st_clean
-from elmo_geo.utils.types import SparkDataFrame
 
 
 # Selected Heritage Inventory for Natural England (SHINE)
@@ -48,13 +45,13 @@ he_shine_raw = SourceDataset(
 )
 
 
-# Listed buildings (LB)
-class HELBRaw(DataFrameModel):
-    """Model for Historic England Listed buildings dataset.
+# Historic England Historic Features (HEHF)
+class HEHFRaw(DataFrameModel):
+    """Model for Historic England Historic Features dataset.
 
     Attributes:
-       list_entry: Reference id for each LB
-       name: Name of the LB
+       list_entry: Reference id for each feature
+       name: Name of the feature
        geometry: Geospatial polygons in EPSG:27700
     """
 
@@ -67,132 +64,54 @@ he_lb_raw = SourceDataset(
     name="he_lb_raw",
     medallion="bronze",
     source="he",
-    model=HELBRaw,
+    model=HEHFRaw,
     restricted=False,
     source_path="/dbfs/mnt/base/unrestricted/source_historic_england_open_data_site/dataset_listed_buildings_polys/format_GEOPARQUET_listed_buildings_polys/LATEST_listed_buildings_polys",
 )
-
-
-# Protected Wreck Sites (PWS)
-class HEPWSRaw(DataFrameModel):
-    """Model for Historic England Sites of Protected Wreck Sites dataset.
-
-    Attributes:
-       list_entry: Reference id for each PWS
-       name: Name of the PWS
-       geometry: Geospatial polygons in EPSG:27700
-    """
-
-    list_entry: int = Field(alias="ListEntry")
-    name: str = Field(alias="Name")
-    geometry: Geometry(crs=SRID) = Field()
 
 
 he_pws_raw = SourceDataset(
     name="he_pws_raw",
     medallion="bronze",
     source="he",
-    model=HEPWSRaw,
+    model=HEHFRaw,
     restricted=False,
     source_path="/dbfs/mnt/base/unrestricted/source_historic_england_open_data_site/dataset_protected_wreck_sites/format_GEOPARQUET_protected_wreck_sites/SNAPSHOT_2024_04_29_protected_wreck_sites/",
 )
-
-
-# Registered Battlefields (RB)
-class HERBRaw(DataFrameModel):
-    """Model for Historic England Registered Battlefields dataset.
-
-    Attributes:
-       list_entry: Reference id for each RB
-       name: Name of the RB
-       geometry: Geospatial polygons in EPSG:27700
-    """
-
-    list_entry: int = Field(alias="ListEntry")
-    name: str = Field(alias="Name")
-    geometry: Geometry(crs=SRID) = Field()
-
 
 he_rb_raw = SourceDataset(
     name="he_rb_raw",
     medallion="bronze",
     source="he",
-    model=HERBRaw,
+    model=HEHFRaw,
     restricted=False,
     source_path="/dbfs/mnt/base/unrestricted/source_historic_england_open_data_site/dataset_registered_battlefields/format_GEOPARQUET_registered_battlefields/SNAPSHOT_2024_04_29_registered_battlefields/",
 )
-
-
-# Registered Parks and Gardens (RPG)
-class HERPGRaw(DataFrameModel):
-    """Model for Historic England Registered Parks and Gardens dataset.
-
-    Attributes:
-       list_entry: Reference id for each RPG
-       name: Name of the RPG
-       geometry: Geospatial polygons in EPSG:27700
-    """
-
-    list_entry: int = Field(alias="ListEntry")
-    name: str = Field(alias="Name")
-    geometry: Geometry(crs=SRID) = Field()
-
 
 he_rpg_raw = SourceDataset(
     name="he_rpg_raw",
     medallion="bronze",
     source="he",
-    model=HERPGRaw,
+    model=HEHFRaw,
     restricted=False,
     source_path="/dbfs/mnt/base/unrestricted/source_historic_england_open_data_site/dataset_registered_parks_and_gardens/format_GEOPARQUET_registered_parks_and_gardens/SNAPSHOT_2024_04_29_registered_parks_and_gardens/",
 )
-
-
-# Scheduled Monuments (SM)
-class HESMRaw(DataFrameModel):
-    """Model for Historic England Scheduled Monuments dataset.
-
-    Attributes:
-       list_entry: Reference id for each SM
-       name: Name of the SM
-       geometry: Geospatial polygons in EPSG:27700
-    """
-
-    list_entry: int = Field(alias="ListEntry")
-    name: str = Field(alias="Name")
-    geometry: Geometry(crs=SRID) = Field()
-
 
 he_sm_raw = SourceDataset(
     name="he_sm_raw",
     medallion="bronze",
     source="he",
-    model=HESMRaw,
+    model=HEHFRaw,
     restricted=False,
     source_path="/dbfs/mnt/base/unrestricted/source_historic_england_open_data_site/dataset_scheduled_monuments/format_GEOPARQUET_scheduled_monuments/SNAPSHOT_2024_04_29_scheduled_monuments/",
 )
-
-
-# World Heritage Sites (WHS)
-class HEWHSRaw(DataFrameModel):
-    """Model for World Heritage Sites dataset.
-
-    Attributes:
-       list_entry: Reference id for each WHS
-       name: Name of the WHS
-       geometry: Geospatial polygons in EPSG:27700
-    """
-
-    list_entry: int = Field(alias="ListEntry")
-    name: str = Field(alias="Name")
-    geometry: Geometry(crs=SRID) = Field()
 
 
 he_whs_raw = SourceDataset(
     name="he_whs_raw",
     medallion="bronze",
     source="he",
-    model=HEWHSRaw,
+    model=HEHFRaw,
     restricted=False,
     source_path="/dbfs/mnt/base/unrestricted/source_historic_england_open_data_site/dataset_world_heritage_sites/format_GEOPARQUET_world_heritage_sites/SNAPSHOT_2024_04_29_world_heritage_sites/",
 )
@@ -214,25 +133,6 @@ class HECombinedSites(DataFrameModel):
     geometry: Geometry(crs=SRID) = Field()
 
 
-def _combine_historic_features(
-    *datasets: list[Dataset],
-    sources: list[str] | None = None,
-) -> SparkDataFrame:
-    """Combine all historic features into a single dataset.
-
-    Parameters:
-        *datasets: Datasets to join together.
-        sources: Dataset shorthand names.
-    """
-    # A partial function that calls combine_long() with provided sources
-    func = partial(combine_long, sources=sources)
-    sdf = func(datasets)
-
-    sdf = sdf.transform(st_clean).withColumn("geometry", F.expr("EXPLODE(ST_Dump(geometry))"))
-
-    return sdf
-
-
 he_combined_sites = DerivedDataset(
     is_geo=True,
     name="he_combined_sites",
@@ -240,9 +140,9 @@ he_combined_sites = DerivedDataset(
     source="he",
     restricted=False,
     func=partial(
-        _combine_historic_features,
+        combine_long,
         sources=[
-            "SHINE",
+            "shine",
             "listed_building",
             "protected_wreck_sites",
             "registered_battlefields",
