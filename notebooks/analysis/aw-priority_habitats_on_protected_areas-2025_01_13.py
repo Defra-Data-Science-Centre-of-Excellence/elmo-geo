4aaee43fb6818e8a0f703927663d923fb4203ed8
Andrew West <andrew.west@defra.gov.uk>
2025-01-15 12:05:45 +0000

    is_woodland_phi (#422)
    Co-authored-by: aw-west <aw-west>

--- notebooks/analysis/aw-priority_habitats_on_protected_areas-2025_01_13.py
+++ notebooks/analysis/aw-priority_habitats_on_protected_areas-2025_01_13.py
@@ -2,29 +2,37 @@
 # MAGIC %md
 # MAGIC # Priority Habitats on Protected Area
 # MAGIC
-# MAGIC |                           | **ha**    | **ha_phi** | **ha_pa** | **ha_pa_phi** |
-# MAGIC | ------------------------- | --------: | ---------: | --------: | ------------: |
-# MAGIC | Assuming PA can overlap   | 9,780,226 |  1,758,497 |   728,992 |       673,833 |
-# MAGIC | Assuming PA can't overlap | 9,780,226 |  1,758,497 |   777,959 |       714,573 |
+# MAGIC |  | Parcel Area (ha)  | Parcel Area that is PHI (ha)  | Parcel Area that is PA (ha)  | Parcel Area that is both (ha)  | PHI on PA (%) | PA on PHI (%) |
+# MAGIC |---|---|---|---|---|---|---|
+# MAGIC | Not PHI | 4,773,744  | 0 | 8,748  | 0 |  |  |
+# MAGIC | PHI | 6,116,808  | 1,758,523  | 1,059,380  | 673,838  | 38.3% | 63.6% |
+# MAGIC | > PHI contains Deciduous Woodland | 3,591,679  | 475,201  | 362,556  | 77,272  | 16.3% | 21.3% |
+# MAGIC | > PHI does not contains Deciduous Woodland | 2,525,129  | 1,283,322  | 696,824  | 596,566  | 46.5% | 85.6% |
+# MAGIC | Total | 10,890,552  | 1,758,523  | 1,068,128  | 673,838  | 38.3% | 63.1% |
 
 # COMMAND ----------
 
 from pyspark.sql import functions as F
+
 from elmo_geo import register
-from elmo_geo.datasets import reference_parcels, protected_areas_parcels, defra_priority_habitat_parcels
+from elmo_geo.datasets import defra_priority_habitat_parcels, protected_areas_parcels, reference_parcels
+
 register()
 
 
-
 sdf = (
-    reference_parcels.sdf().select("id_parcel", "area_ha")
+    reference_parcels.sdf()
+    .select("id_parcel", "area_ha")
     .join(
         (
             defra_priority_habitat_parcels.sdf()
             # Priority Habitats geometries have multiple habitats, they are split for each habitat_name, and unioned here.
             .groupby("id_parcel", "fid")
-            .agg(F.expr("FIRST(proportion) AS proportion"))
-            .groupby("id_parcel")
+            .agg(
+                F.expr("ARRAY_CONTAINS(COLLECT_LIST(habitat_name), 'Deciduous woodland') AS is_woodland"),
+                F.expr("FIRST(proportion) AS proportion"),
+            )
+            .groupby("id_parcel", "is_woodland")
             # Priority Habitats shouldn't overlap, so we sum them, but clip to 1 because there will be rounding errors.
             .agg(F.expr("LEAST(1, SUM(proportion)) AS p_phi"))
         ),
@@ -41,26 +49,20 @@ sdf = (
                 "proportion",
             )
             .groupby("id_parcel")
-            # Protected Areas can overlap, so we calculate the probablistic area that is in any.
-            #.agg(F.expr("1 - EXP(SUM(LOG(1 - proportion))) AS p_pa"))
+            # Protected Areas can overlap, so we calculate the probabilistic area that is in any.
+            .agg(F.expr("1 - EXP(SUM(LOG(1 - proportion))) AS p_pa"))
             # Assuming they don't overlap
-            .agg(F.expr("LEAST(1, SUM(proportion)) AS p_pa"))
+            # .agg(F.expr("LEAST(1, SUM(proportion)) AS p_pa"))
         ),
         on="id_parcel",
         how="outer",
     )
-    .selectExpr(
-        "id_parcel",
-        "area_ha",
-        "COALESCE(p_phi, 0) AS p_phi",
-        "COALESCE(p_pa, 0) AS p_pa",
-    )
-    .groupby()
+    .groupby("is_woodland")
     .agg(
-        F.expr("SUM(area_ha) AS ha"),  # Sum of parcel areas
-        F.expr("SUM(area_ha * p_phi) AS ha_phi"),  # Sum of parcel areas that is Priority Habitat
-        F.expr("SUM(area_ha * p_pa) AS ha_pa"),  # Sum of parcel areas that is Protected Area
-        F.expr("SUM(area_ha * p_pa * p_phi) AS ha_pa_phi"),  # Sum of parcel areas this is Protected Area and Priority Habitat
+        F.expr("ROUND(SUM(area_ha)) AS ha"),  # Sum of parcel areas
+        F.expr("ROUND(SUM(area_ha * p_phi)) AS ha_phi"),  # Sum of parcel areas that is Priority Habitat
+        F.expr("ROUND(SUM(area_ha * p_pa)) AS ha_pa"),  # Sum of parcel areas that is Protected Area
+        F.expr("ROUND(SUM(area_ha * p_pa * p_phi)) AS ha_pa_phi"),  # Sum of parcel areas this is Protected Area and Priority Habitat
     )
 )
 
