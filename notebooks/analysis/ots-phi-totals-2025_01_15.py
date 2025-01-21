2ea254b8788a0eeef6dfa9b36253f866bec5c5f3
obi-defra <133129211+obi-defra@users.noreply.github.com>
2025-01-21 16:41:41 +0000

    fmt

--- notebooks/analysis/ots-phi-totals-2025_01_15.py
+++ notebooks/analysis/ots-phi-totals-2025_01_15.py
@@ -9,15 +9,14 @@
 
 import pyspark.sql.functions as F
 
+from elmo_geo import register
 from elmo_geo.datasets import (
     defra_priority_habitat_england_raw,
     defra_priority_habitat_parcels,
     reference_parcels,
 )
-
 from elmo_geo.io.file import auto_repartition
 from elmo_geo.st.join import sjoin
-from elmo_geo import register
 
 register()
 
@@ -28,44 +27,47 @@ defra_priority_habitat_england_raw.sdf().display()
 # COMMAND ----------
 
 # total area of raw phi geometries - 2,294,282
-(defra_priority_habitat_england_raw.sdf()
- .dropDuplicates(subset=["geometry"])
- .agg(F.sum("areaha")).display())
+(defra_priority_habitat_england_raw.sdf().dropDuplicates(subset=["geometry"]).agg(F.sum("areaha")).display())
 
 # COMMAND ----------
 
 # area of phi on parcels - 1,758,555 ha
-(reference_parcels.sdf().join(
-    defra_priority_habitat_parcels.sdf().dropDuplicates(subset=["id_parcel", "fid"]), 
-    on="id_parcel", 
-    how="left",)
- .withColumn("phi_area", F.expr("proportion*area_ha"))
- .agg(F.sum("phi_area")).display())
+(
+    reference_parcels.sdf()
+    .join(
+        defra_priority_habitat_parcels.sdf().dropDuplicates(subset=["id_parcel", "fid"]),
+        on="id_parcel",
+        how="left",
+    )
+    .withColumn("phi_area", F.expr("proportion*area_ha"))
+    .agg(F.sum("phi_area"))
+    .display()
+)
 
 # COMMAND ----------
 
 # check that phi geometries don't overlap
 # expect query to return zero (or small) area
 # result - 932 ha
-(sjoin(defra_priority_habitat_england_raw.sdf(), defra_priority_habitat_england_raw.sdf())
- .filter("fid_left <> fid_right")
- .transform(auto_repartition, count_ratio=1e-5)
- .withColumn("geometry_left", F.expr("ST_CollectionExtract(geometry_left, 3)"))
- .withColumn("geometry_right", F.expr("ST_CollectionExtract(geometry_right, 3)"))
- .withColumn("ha_intersection", 
-             F.expr("ST_Area(ST_Intersection(geometry_left, geometry_right)) / 10000"))
- .agg(F.sum("ha_intersection"))
+(
+    sjoin(defra_priority_habitat_england_raw.sdf(), defra_priority_habitat_england_raw.sdf())
+    .filter("fid_left <> fid_right")
+    .transform(auto_repartition, count_ratio=1e-5)
+    .withColumn("geometry_left", F.expr("ST_CollectionExtract(geometry_left, 3)"))
+    .withColumn("geometry_right", F.expr("ST_CollectionExtract(geometry_right, 3)"))
+    .withColumn("ha_intersection", F.expr("ST_Area(ST_Intersection(geometry_left, geometry_right)) / 10000"))
+    .agg(F.sum("ha_intersection"))
 ).display()
 
 # COMMAND ----------
 
 # checking overlap between geometries with the same id - approximately matches the phi total 2,294,282 vs 2,294,127
-(sjoin(defra_priority_habitat_england_raw.sdf(), defra_priority_habitat_england_raw.sdf())
- .filter("fid_left = fid_right")
- .transform(auto_repartition, count_ratio=1e-5)
- .withColumn("geometry_left", F.expr("ST_CollectionExtract(geometry_left, 3)"))
- .withColumn("geometry_right", F.expr("ST_CollectionExtract(geometry_right, 3)"))
- .withColumn("ha_intersection", 
-             F.expr("ST_Area(ST_Intersection(geometry_left, geometry_right)) / 10000"))
- .agg(F.sum("ha_intersection"))
+(
+    sjoin(defra_priority_habitat_england_raw.sdf(), defra_priority_habitat_england_raw.sdf())
+    .filter("fid_left = fid_right")
+    .transform(auto_repartition, count_ratio=1e-5)
+    .withColumn("geometry_left", F.expr("ST_CollectionExtract(geometry_left, 3)"))
+    .withColumn("geometry_right", F.expr("ST_CollectionExtract(geometry_right, 3)"))
+    .withColumn("ha_intersection", F.expr("ST_Area(ST_Intersection(geometry_left, geometry_right)) / 10000"))
+    .agg(F.sum("ha_intersection"))
 ).display()
