8c4a7530cb81d46cedd576f3a5d8f0060a80fcf9
aw-west <andrew.west@defra.gov.uk>
2025-02-03 12:01:18 +0000

    merge issue

--- elmo_geo/etl/etl.py
+++ elmo_geo/etl/etl.py
@@ -304,7 +304,7 @@ class SourceDataset(TabularDataset):
             return df
         # Replace "name" with "_name", so we can replace "useful_name" with "name" and not have duplicate columns.
         mapping_old = {field.original_name: "_" + field.original_name for _, field in self.model.__fields__.values() if field.alias is not None}
-        mapping = {field.alias: field.original_name for _, field in self.model.__fields__.values() if field.alias is not None if field.alias is not None}
+        mapping = {field.alias: field.original_name for _, field in self.model.__fields__.values() if field.alias is not None}
         if isinstance(df, SparkDataFrame):
             return df.withColumnsRenamed(mapping_old).withColumnsRenamed(mapping)
         else:
