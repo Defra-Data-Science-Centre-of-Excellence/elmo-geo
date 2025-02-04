d4c476e4add0903c301e2b77fcc0853227d72267
Andrew West <andrew.west@defra.gov.uk>
2025-02-04 13:36:53 +0000

    Expand phi on pa for all habitats (#444)
    Co-authored-by: aw-west <aw-west>

--- notebooks/analysis/aw-priority_habitats_on_protected_areas-2025_01_13.py
+++ notebooks/analysis/aw-priority_habitats_on_protected_areas-2025_01_13.py
@@ -11,13 +11,20 @@
 
 # COMMAND ----------
 
+from pyspark.sql import DataFrame as SparkDataFrame
 from pyspark.sql import functions as F
 
 from elmo_geo import register
-from elmo_geo.datasets import defra_priority_habitat_parcels, protected_areas_parcels, reference_parcels
+from elmo_geo.datasets import (
+    defra_priority_habitat_parcels,
+    protected_areas_parcels,
+    reference_parcels,
+    rpa_hedges_raw,  # wfm_parcels,
+)
 
 register()
 
+# COMMAND ----------
 
 sdf = (
     reference_parcels.sdf()
@@ -28,32 +35,45 @@ sdf = (
     #     how="right",
     # )
     .join(
-        (
+        SparkDataFrame.unionByName(
+            rpa_hedges_raw.sdf().selectExpr(
+                "CONCAT(sheet_id, parcel_ref) AS id_parcel",
+                "'hedges' AS habitat_name",
+                "ST_Length(geometry) AS m",
+            ),
             defra_priority_habitat_parcels.sdf()
-            # Priority Habitats geometries have multiple habitats, they are split for each habitat_name, and unioned here.
-            .groupby("id_parcel", "fid")
-            .agg(
-                F.expr("ARRAY_CONTAINS(COLLECT_LIST(habitat_name), 'Deciduous woodland') AS is_woodland"),
-                F.expr("FIRST(proportion) AS proportion"),
+            .groupby(
+                "id_parcel",
+                "habitat_name",
             )
-            .groupby("id_parcel", "is_woodland")
-            # Priority Habitats shouldn't overlap, so we sum them, but clip to 1 because there will be rounding errors.
-            .agg(F.expr("LEAST(1, SUM(proportion)) AS p_phi"))
+            .agg(
+                F.expr("LEAST(1, SUM(proportion)) AS p_phi"),
+            ),
+            allowMissingColumns=True,
         ),
         on="id_parcel",
         how="outer",
     )
     .join(
-        protected_areas_parcels.sdf().selectExpr("id_parcel", "proportion_any AS p_pa"),
+        (
+            protected_areas_parcels.sdf().selectExpr(
+                "id_parcel",
+                "proportion_any AS p_pa",
+            )
+        ),
         on="id_parcel",
         how="outer",
     )
-    .groupby("is_woodland")
+    .selectExpr(
+        "id_parcel",
+        "habitat_name",
+        "COALESCE(area_ha * p_phi, m) AS v_hab",
+        "p_pa",
+    )
+    .groupby("habitat_name")
     .agg(
-        F.expr("ROUND(SUM(area_ha)) AS ha"),  # Sum of parcel areas
-        F.expr("ROUND(SUM(area_ha * p_phi)) AS ha_phi"),  # Sum of parcel areas that is Priority Habitat
-        F.expr("ROUND(SUM(area_ha * p_pa)) AS ha_pa"),  # Sum of parcel areas that is Protected Area
-        F.expr("ROUND(SUM(area_ha * p_pa * p_phi)) AS ha_pa_phi"),  # Sum of parcel areas this is Protected Area and Priority Habitat
+        F.expr("SUM(v_hab) AS v_hab"),
+        F.expr("SUM(v_hab * p_pa) AS v_pa"),
     )
 )
 
